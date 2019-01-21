3fc1af8c820e15fc4c4dc91ca79b7d66be8592d6
Daniel Cote <dccote@novajo.ca>
2019-01-21 16:00:00 -0500

    Attempting better doc and extended matrices for axicon

--- ABCD.py
+++ ABCD.py
@@ -39,6 +39,8 @@ class Matrix(object):
 	def __mul__(self, rightSide):
 		if isinstance(rightSide, Matrix):
 			return self.mul_matrix(rightSide)
+		elif isinstance(rightSide, Axicon):
+			return self.mul_axicon(rightSide)
 		elif isinstance(rightSide, Ray):
 			return self.mul_ray(rightSide)
 		else:
@@ -94,6 +96,7 @@ class Aperture(Matrix):
 
 		plt.arrow(z, halfHeight+1, 0,-1, width=0.1, fc='k', ec='k',head_length=0.05, head_width=1,length_includes_head=True)
 		plt.arrow(z, -halfHeight-1, 0, 1, width=0.1, fc='k', ec='k',head_length=0.05, head_width=1, length_includes_head=True)
+
 
 class OpticalPath(object):
 	def __init__(self):
@@ -182,6 +185,46 @@ class OpticalPath(object):
 			# else: # ray will simply stop drawing from here
 			
 		return (x,y)
+
+
+class ExtendedMatrix(Matrix):
+	def __init__(self, A, B, C, D, E, F, physicalLength, apertureDiameter=float('+Inf')):	
+		self.E = E
+		self.F = F
+		super(ExtendedMatrix, self).__init__(A, B, C,D, physicalLength, apertureDiameter)
+
+	def __init__(self, matrix):	
+		self.E = 0
+		self.F = 0
+		super(ExtendedMatrix, self).__init__(matrix.A, matrix.B, matrix.C,matrix.D, matrix.L, matrix.apertureDiameter)
+
+	def __mul__(self, rightSide):
+		matrix = super.__mul__(self, rightSide)
+
+		if isinstance(rightSide, ExtendedMatrix):
+			matrix = ExtendedMatrix(matrix)
+			matrix.E = rightSide.E
+			matrix.F = rightSide.F
+		
+		return matrix
+
+
+class Axicon(ExtendedMatrix):
+	def __init__(self, alpha, n, diameter=float('+Inf')):	
+		self.E = 0
+		self.F = (n-1)*alpha
+
+		super(Axicon, self).__init__(A=1, B=0, C=0,D=1, physicalLength=0, apertureDiameter=diameter)
+
+	def drawAt(self, z, axes):
+		halfHeight = 4
+		if self.apertureDiameter != float('Inf'):
+			halfHeight = self.apertureDiameter/2
+
+		plt.arrow(z, 0, 0, halfHeight, width=0.1, fc='k', ec='k',head_length=0.25, head_width=0.25,length_includes_head=True)
+		plt.arrow(z, 0, 0, -halfHeight, width=0.1, fc='k', ec='k',head_length=0.25, head_width=0.25, length_includes_head=True)
+
+
 
 if __name__ == "__main__":
 	print("")
--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # RayTracing
 This code aims to provide a simple ray tracing module for calculating various properties of optical paths (object, image, aperture stops, field stops).  It makes use of ABCD matrices and does not consider aberrations (spherical or chromatic). It is not a package to do "Rendering in 3D with raytracing".  
 
-The code has been developed first for teaching purposes and is used in my "[Optique](https://itunes.apple.com/ca/book/optique/id949326768?mt=11)" Study Notes (french only).  but also for actual use in my research. I have made no attempts at making high performance code.  Readability and simplicity of usage is the key here. It is a single file with only `matplotlib` as a dependent module.
+The code has been developed first for teaching purposes and is used in my "[Optique](https://itunes.apple.com/ca/book/optique/id949326768?mt=11)" Study Notes (french only), but also for actual use in my research. I have made no attempts at making high performance code.  Readability and simplicity of usage is the key here. It is a single file with only `matplotlib` as a dependent module.
 
 The module defines `Ray` ,  `Matrix` and `OpticalPath` as the main elements.  An optical path is a sequence of matrices into which rays will propagate. Specific subclasses of `Matrix` exists: `Space`, `Lens` and `Aperture`. Finally, a ray fan is a collection of rays, originating from a given point with a range of angles.
 
 ## Getting started
+You need `matplotlib`, which is a fairly standard Python module. If you do not have it,  installing [Anaconda](https://www.anaconda.com/download/) is your best option.
+
 You create an OpticalPath, which you then populate with optical elements such as Space, Lens or Aperture. You can then adjust the optical path properties (object height for instance) and display in matplotlib.
 
 This will show you a few examples of things you can do:
