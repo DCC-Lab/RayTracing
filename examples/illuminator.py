0d17e61c42bbc1526397d2e6f47e47d0fa2467af
Daniel Cote <dccote@novajo.ca>
2019-02-07 22:45:34 -0500

    Update illuminator.py
    With labels. Don't display that well though. Need to fix proper position.

--- examples/illuminator.py
+++ examples/illuminator.py
@@ -10,15 +10,15 @@ path.objectHeight = 1.0
 path.fanAngle = 0.5
 path.rayNumber = 3
 path.append(Space(d=4))
-path.append(Lens(f=4,diameter=2.5))
+path.append(Lens(f=4,diameter=2.5, label='Collector'))
 path.append(Space(d=4+25))
-path.append(Lens(f=25, diameter=7.5))
+path.append(Lens(f=25, diameter=7.5, label='Condenser'))
 path.append(Space(d=20))
 path.append(Space(d=5))
 path.append(Space(d=9))
-path.append(Lens(f=9, diameter=8))
+path.append(Lens(f=9, diameter=8, label='objective'))
 path.append(Space(d=9))
-
+path.showLabels=True
 print(path.fieldStop())
 print(path.fieldOfView())
 path.display(onlyChiefAndMarginalRays=True, limitObjectToFieldOfView=True)
