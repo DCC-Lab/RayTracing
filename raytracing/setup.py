7de949a25fe7b0b7e57b7ad14d35e829f31d75f0
Daniel Cote <dccote@novajo.ca>
2019-02-12 22:15:21 -0500

    MOstly complete python package

--- raytracing/setup.py
+++ raytracing/setup.py
@@ -6,14 +6,29 @@ setuptools.setup(
     url="https://github.com/DCC-Lab/RayTracing",
     author="Daniel Cote",
     author_email="dccote@cervo.ulaval.ca",
-    description="An optical design tool based on ray matrices\
-    (i.e. ABCD matrices) to model propagation and trace rays through\
-    optical elements, with or without apertures.",
+    description="Simple optical ray tracing library \
+    to validate the design of an optical system\
+    (lenses positions and sizes, focal lengths).",
     long_description=open('README.md').read(),
-    packages=setuptools.find_packages(),
+    long_description_content_type='text/markdown',
+    packages=['abcd', 'objectives', 'axicon'],
     install_requires=[],
+    license='MIT',
+    keywords='optics lenses ray matrices aperture field stop',
+    py_modules=['abcd', 'objectives', 'axicon'],
+    install_requires=['matplotlib'],
+    python_requires='>=3',
     classifiers=[
         'Development Status :: 4 - Beta',
+        # Indicate who your project is intended for
+        'Intended Audience :: Scientists',
+        'Topic :: Optics :: Design Tools',
+
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
