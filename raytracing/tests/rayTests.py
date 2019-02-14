fa88b13fc7f94ced80ce3f25271b8a8cd799ead7
Daniel Cote <dccote@novajo.ca>
2019-02-14 02:12:42 -0500

    Update rayTests.py

--- raytracing/tests/rayTests.py
+++ raytracing/tests/rayTests.py
@@ -4,7 +4,7 @@ from raytracing import *
 
 inf = float("+inf")
 
-class TestRayTracing(unittest.TestCase):
+class TestRay(unittest.TestCase):
     def testRay(self):
         ray = Ray()
         self.assertIsNotNone(Ray())
@@ -14,9 +14,16 @@ class TestRayTracing(unittest.TestCase):
         self.assertEqual(ray.apertureDiameter, inf)
         self.assertFalse(ray.isBlocked)
         self.assertTrue(ray.isNotBlocked)
+
     def testFan(self):
         fan = Ray.fan(y=0, radianMin=-0.1, radianMax=0.1, N=5)
         self.assertIsNotNone(fan)                
+        self.assertEqual(len(fan), 5)
+        self.assertEqual(min(map(lambda r: r.theta, fan)), -0.1)        
+        self.assertEqual(max(map(lambda r: r.theta, fan)), 0.1)        
+        self.assertEqual(min(map(lambda r: r.y, fan)), 0)        
+        self.assertEqual(max(map(lambda r: r.y, fan)), 0)        
+    
     def testFanGroup(self):
         fanGroup = Ray.fanGroup(yMin=0, yMax=1, M=10, radianMin=-0.1, radianMax=0.1, N=5)
         self.assertIsNotNone(fanGroup)                
