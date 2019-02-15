850be154ea02268bb26f48344227e580807bddc3
Daniel Cote <dccote@novajo.ca>
2019-02-14 22:12:55 -0500

    Fixed up version number to address label sizes

--- raytracing/__init__.py
+++ raytracing/__init__.py
@@ -5,7 +5,7 @@ from .abcd import *
 from .objectives import *
 from .axicon import *
 
-__version__ = "1.0.5"
+__version__ = "1.0.6"
 __author__ = "Daniel Cote <dccote@cervo.ulaval.ca>"
 print("Main documentation at https://github.com/DCC-Lab/RayTracing")
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="raytracing",
-    version="1.0.5",
+    version="1.0.6",
     url="https://github.com/DCC-Lab/RayTracing",
     author="Daniel Cote",
     author_email="dccote@cervo.ulaval.ca",
