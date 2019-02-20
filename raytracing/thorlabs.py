13dba01d54dd1fee95c78ce86a4d9ba74d7b735a
Daniel Cote <dccote@novajo.ca>
2019-02-20 18:05:18 -0500

    Renamed classes.

--- raytracing/thorlabs.py
+++ raytracing/thorlabs.py
@@ -2,8 +2,8 @@ from .abcd import *
 from math import  *
 import matplotlib.transforms as transforms
 
-class ThorlabsAchromatLens(MatrixGroup):
-    """ From https://www.thorlabs.com/newgrouppage9.cfm?objectgroup_id=120 
+class DoubletAchromatLens(MatrixGroup):
+    """ Nomenclature from https://www.thorlabs.com/newgrouppage9.cfm?objectgroup_id=120 
 
     """
 
@@ -23,7 +23,7 @@ class ThorlabsAchromatLens(MatrixGroup):
         elements.append(DielectricInterface(n1=n1, n2=n2, R=R2, diameter=diameter))
         elements.append(Space(d=tc2))
         elements.append(DielectricInterface(n1=n2, n2=1, R=R3, diameter=diameter))
-        super(ThorlabsAchromatLens, self).__init__(elements=elements)
+        super(DoubletAchromatLens, self).__init__(elements=elements)
 
         # After having built the lens, we confirm that the expected effective
         # focal length (fa) is actually within 1% of the calculated focal length
@@ -33,12 +33,12 @@ class ThorlabsAchromatLens(MatrixGroup):
                 expected {1:.4}".format(f, fa))
 
 
-class AC254_050_A(ThorlabsAchromatLens):
+class AC254_050_A(DoubletAchromatLens):
     def __init__(self):
         super(AC254_050_A,self).__init__(fa=50.2,fb=43.4, R1=33.3,R2=-22.28, R3=-291.07, 
                                     tc1=9, tc2=2.5, n1=1.6700, n2=1.7283, diameter=25.4)
 
-class AC254_045_A(ThorlabsAchromatLens):
+class AC254_045_A(DoubletAchromatLens):
     def __init__(self):
         super(AC254_045_A,self).__init__(fa=45.0,fb=40.2, R1=31.2, R2=-25.90, R3=-130.6, 
                                     tc1=7, tc2=2.0, n1=1.6700, n2=1.8052, diameter=25.4)
