f547f0ece18b5361daa91d97eceaa5d5d81f1a8b
Daniel Cote <dccote@novajo.ca>
2019-02-20 22:12:57 -0500

    Update olympus.py

--- raytracing/olympus.py
+++ raytracing/olympus.py
@@ -13,7 +13,7 @@ class LUMPlanFL40X(Objective):
                                          focusToFocusLength=40,
                                          backAperture=7,
                                          workingDistance=2,
-                                         label='LUMPLFL40X',
+                                         label='LUMPlanFL40X',
                                          url="https://www.edmundoptics.com/p/olympus-lumplfln-40xw-objective/3901/")
 
 class XLUMPlanFLN20X(Objective):
@@ -44,43 +44,3 @@ class MVPlapo2XC(Objective):
                                          workingDistance=20,
                                          label='MVPlapo2XC',
                                          url="")
-
-if __name__ == "__main__":
-    obj = Objective(f=10, NA=0.8, focusToFocusLength=60, backAperture=18, workingDistance=2, label="Objective")
-    print("Focal distances: ", obj.focalDistances())
-    print("Position of PP1 and PP2: ", obj.principalPlanePositions(z=0))
-    print("Focal spots positions: ", obj.focusPositions(z=0))
-    print("Distance between entrance and exit planes: ", obj.L)
-
-    path1 = OpticalPath()
-    path1.fanAngle = 0.0
-    path1.fanNumber = 1
-    path1.rayNumber = 15
-    path1.objectHeight = 10.0
-    path1.label = "Path with objective"
-    path1.append(Space(180))
-    path1.append(obj)
-    path1.append(Space(10))
-    path1.display()
-
-    path2 = OpticalPath()
-    path2.fanAngle = 0.0
-    path2.fanNumber = 1
-    path2.rayNumber = 15
-    path2.objectHeight = 10.0
-    path2.label = "Path with LUMPLFL40X"
-    path2.append(Space(180))
-    path2.append(LUMPLFL40X())
-    path2.append(Space(10))
-    path2.display()
-
-    path3 = OpticalPath()
-    path3.fanAngle = 0.0
-    path3.fanNumber = 1
-    path3.rayNumber = 15
-    path3.objectHeight = 10.0
-    path3.label = "Path with MVPlapo2XC"
-    path3.append(Space(180))
-    path3.append(MVPlapo2XC())
-    path3.append(Space(10))
-    path3.display()
