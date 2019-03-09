8e545f227bded4972942c0f1c02c83016f0f5da1
Daniel Cote <dccote@novajo.ca>
2019-03-09 00:14:44 -0500

    Update for 1.1.1

--- test.py
+++ test.py
@@ -3,6 +3,10 @@ from raytracing.thorlabs import *
 
 #DielectricInterface(n1=1,n2=1.5, R=10).display()
 thorlabs.AC508_150_B().display()
+thorlabs.AC254_050_A().display()
+eo.PN_33_921().display()
+olympus.LUMPlanFL40X().display()
+
 
 ThickLens(n=1.5, R1=10, R2=-20, thickness=4, label='Thick Lens').display()
 eo.PN_33_921().display()
