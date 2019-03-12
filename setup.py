f66c4ffaebb8b31f21c6222626d0c6bf31104fa0
Daniel Cote <dccote@novajo.ca>
2019-03-12 11:02:37 -0400

    Update 1.1.4

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ import setuptools
 
 setuptools.setup(
     name="raytracing",
-    version="1.1.3",
+    version="1.1.4",
     url="https://github.com/DCC-Lab/RayTracing",
     author="Daniel Cote",
     author_email="dccote@cervo.ulaval.ca",
