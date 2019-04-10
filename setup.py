0bff9a4a5677d5ed67b9dc99e31decf596f43ffc
Daniel Cote <dccote@novajo.ca>
2019-04-10 08:23:32 -0400

    Improvements and 1.1.6

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ rm dist/*; python setup.py sdist bdist_wheel; python -m twine upload dist/*
 
 setuptools.setup(
     name="raytracing",
-    version="1.1.5",
+    version="1.1.6",
     url="https://github.com/DCC-Lab/RayTracing",
     author="Daniel Cote",
     author_email="dccote@cervo.ulaval.ca",
