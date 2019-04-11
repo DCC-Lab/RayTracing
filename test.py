5fa80f237798c4dc412df12617b198827fcae08d
Daniel Cote <dccote@novajo.ca>
2019-04-11 08:19:39 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,4 @@
 from raytracing import *
-from raytracing.thorlabs import *
-from raytracing.eo import *
-from raytracing.olympus import *
-
-# PN_85_877().display()
-# PN_85_877().flipOrientation().display()
-# AC254_050_B().display()
-# AC254_050_B().flipOrientation().display()
-# XLUMPlanFLN20X().display()
-# XLUMPlanFLN20X().flipOrientation().display()
-
-cavity = LaserPath()
-cavity.inputBeam = GaussianBeam(w=0.01)
-cavity.append(Space(d=160))
-cavity.append(Lens(f=50))
-cavity.append(Space(d=160))
-cavity.display()
 
 cavity = LaserPath()
 cavity.append(Space(d=160))
