6a69277d9574f6ea52cf721949d76eb75818e6a1
Daniel Côté <dccote@novajo.ca>
2020-02-16 16:03:02 -0500

    Update test.py
    Improved with progress

--- test.py
+++ test.py
@@ -3,28 +3,42 @@ from numpy import *
 import matplotlib.pyplot as plt
 
 fobj = 5
+dObj = 5
+
 f2 = 200
-f3 = 200
+d2 = 100
+
+f3 = 100
+d3 = 10
 
 path = ImagingPath()
 path.append(Space(d=f3))
-path.append(Lens(f=f3, diameter=100))
+path.append(Lens(f=f3, diameter=d3))
 path.append(Space(d=f3))
 path.append(Space(d=f2))
-path.append(Lens(f=f2, diameter=100))
+path.append(Lens(f=f2, diameter=d2))
 path.append(Space(d=f2))
 path.append(Space(d=fobj))
-path.append(Lens(f=fobj, diameter=5))
+path.append(Lens(f=fobj, diameter=dObj))
 path.append(Space(d=fobj))
 
 rayHeights = []
-for ray in RandomLambertianRays(yMax=2.5,M=1000000):
+nRays = 1000000
+i = 0
+progressLog = 100
+
+allRays = RandomLambertianRays(yMax=2.5,M=nRays)
+
+for ray in allRays:
     lastRay = path.traceThrough(ray)
     if lastRay.isNotBlocked:
         rayHeights.append(lastRay.y)
 
-_ = plt.hist(rayHeights, bins=40,density=True)
+    if i % progressLog == 0:
+        progressLog *= 10
+        print("Progress {0}/{1} ({2}%) ".format(i, nRays,i/nRays*100))
+    i += 1
+
+plt.hist(rayHeights, bins=40,density=True)
 plt.title("Intensity profile")
 plt.show()
-
-#print(totalNumberRays)
