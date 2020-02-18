817ae176ce954a4b35d10df5c1b42cb565fd35dc
Daniel Côté <dccote@novajo.ca>
2020-02-18 04:16:58 -0500

    Complete refactoring of Rays
    It appeared clear that Rays should use a list and provide all the methods for computing the histograms when needed.  Since we only simulate millions of rays, it only takes a few megabytes of memory to keep all rays, so that does not affect performance that much.
    Also, we will start implementing an "intensity" for Ray() to accelerate the simulations: for histograms, this will be used as a weight instead of having to simulate a number of rays proportionnal to the intensity at that point.  This "wavepacket" strategy is common for Monte Carlo.

--- raytracing/rays.py
+++ raytracing/rays.py
@@ -2,35 +2,133 @@ from .ray import *
 from numpy import *
 import matplotlib.pyplot as plt
 
+""" A group of rays kept as a list, to be used as a starting
+point (i.e. an object) or as a cumulative detector (i.e. at an image
+or output plane) for ImagingPath, MatrixGroup or any tracing function.
+Subclasses can provide a computed ray for Monte Carlo simulation.
+"""
+
 class Rays:
-    def __init__(self, rays):
+    def __init__(self, rays=[], histogramOnly=False):
         self.rays = rays
-        if rays is not None:
-            self.N = len(rays)
-        else:
-            self.N = None
-            
         self.iteration = 0
+        self.histogramOnly = histogramOnly
+
+        self._yValues = None
+        self._thetaValues = None
+        self._intensityProfile = None
+        self._intensityBinEdges = None
+        self._directionProfile = None
+        self._directionBinEdges = None
 
     @property
     def count(self):
-        return self.N
+        return len(self.rays)
+
+    @property
+    def yValues(self):
+        if self._yValues is None:
+            self._yValues = list(map(lambda x : x.y, self.rays))
+
+        return self._yValues
+
+    @property
+    def thetaValues(self):
+        if self._thetaValues is None:
+            self._thetaValues = list(map(lambda x : x.theta, self.rays))
+
+        return self._thetaValues
+    
+    def intensityProfile(self, binCount=None, min=None, max=None):
+        if self._intensityProfile is None:
+            if binCount is None:
+                binCount = 'auto'
+
+            if min is None:
+                min = self.yValues.min()
+
+            if max is None:
+                max = self.yValues.max()
+
+            (self._intensityProfile, self._intensityBinEdges) = 
+                numpy.histogram(self.yValues, bins=binCount, range=(min, max))
+
+        return (self._intensityProfile, self._intensityBinEdges)
+
+
+    def directionProfile(self, binCount=None, min=None, max=None):
+        if self._directionProfile is None:
+            if binCount is None:
+                binCount = 'auto'
+
+            if min is None:
+                min = self.thetaValues.min()
+
+            if max is None:
+                max = self.thetaValues.max()
+
+            (self._directionProfile, self._directionBinEdges) = 
+                numpy.histogram(self.thetaValues, bins=binCount, range=(min, max))
+
+        return (self._directionProfile, self._directionBinEdges)
+
+    def display(self, title="Intensity profile"):
+        plt.ioff()
+        plt.plot(self.intensityBinEdges, self.intensityProfile, fmt="ko-")
+        plt.xlabel("Distance")
+        plt.ylabel("Intensity [arb.u]")
+        plt.title(title)
+        plt.show()
     
     def __iter__(self):
+        self.iteration = 0
         return self
 
     def __next__(self) -> Ray :
-        if self.rays is None:
+        if self.iteration < len(self.rays):
+            ray = self.rays[self.iteration]
             self.iteration += 1
-            return self.computeNext()
-        elif len(self.rays) == 0:
-            raise StopIteration
-        else:
-            self.iteration += 1
-            return self.rays.pop()
+            return ray
 
-    def computeNext(self) -> Ray :
-        raise StopIteration 
+        raise StopIteration
+
+    def add(self, ray):
+        if self.histogramOnly:
+            raise NotImplemented
+        else
+            self.rays.append(ray)
+
+            # Invalidate cached values
+            self._yValues = None
+            self._thetaValues = None
+            self._intensityProfile = None
+            self._intensityBinEdges = None
+            self._directionProfile = None
+            self._directionBinEdges = None
+
+    def whichBin(self, value):
+        if value <= self.min:
+            return 0
+        elif value >= self.max:
+            return len(self.distribution)-1
+        
+        return int((value - self.min)/self.delta)
+
+
+    # @property
+    # def intensityError(self):
+    #     return list(map(lambda x : sqrt(x), self.distribution))
+
+    # @property
+    # def normalizedIntensity(self):
+    #     maxValue = max(self.values)
+    #     return list(map(lambda x : x/maxValue, self.distribution))
+
+    # @property
+    # def normalizedIntensityError(self):
+    #     maxValue = max(self.distribution)
+    #     return list(map(lambda x : x/maxValue, self.error))
+
 
 class UniformRays(Rays):
     def __init__(self, yMax=1.0, yMin=None, thetaMax=pi/2, thetaMin=None, M=1000, N=1000):
@@ -51,11 +149,6 @@ class UniformRays(Rays):
                 rays.append(Ray(y,theta))
         super(UniformRays, self).__init__(rays=rays)
 
-    @property
-    def count(self):
-        return self.N*self.M
-
-
 class LambertianRays(Rays):
     def __init__(self, yMax, yMin=None, M=100, N=100, I=100):
         self.yMax = yMax
@@ -70,15 +163,11 @@ class LambertianRays(Rays):
         self.I = I
         rays = []
         for theta in linspace(self.thetaMin, self.thetaMax, N, endpoint=True):
-            intensity = int( I * cos(theta) * cos(theta) )
+            intensity = int( I * cos(theta) )
             for y in linspace(yMin, yMax, M, endpoint=True):
                 for k in range(intensity):
-                    rays.append(Ray(y,theta))
+                    rays.append(Ray(y,theta, intensity))
         super(LambertianRays, self).__init__(rays=rays)
-
-    @property
-    def count(self):
-        return self.N*self.M*self.I
 
 class RandomLambertianRays(Rays):
     def __init__(self, yMax, yMin=None, M=10000):
@@ -97,68 +186,20 @@ class RandomLambertianRays(Rays):
     def count(self):
         return self.M
 
-    def computeNext(self) -> Ray :
+    def __next__(self) -> Ray :
         if self.iteration >= self.M:
             raise StopIteration 
 
         theta = 0
+        intensity = 1.0
         while (True):
             theta = self.thetaMin + random.random() * (self.thetaMax - self.thetaMin)
-            threshold = cos(theta)
-            threshold *= threshold
+            intensity = cos(theta)
             seed = random.random()
-            if seed < threshold:
+            if seed < intensity:
                 break
 
         y = self.yMin + random.random() * (self.yMax - self.yMin) 
-        return Ray(y,theta)
-
-
-class RayHistogram:
-    def __init__(self, min=None, max=None, binCount=10):
-        self.values = [0] * binCount
-        self.min = min
-        self.max = max
-        self.range = linspace(min, max, binCount)
-        self.width = abs(max - min)
-        self.delta = self.width/binCount
-        self.binCount = binCount
-
-    @property
-    def error(self):
-        return list(map(lambda x : sqrt(x), self.values))
-
-    @property
-    def normalizedValues(self):
-        maxValue = max(self.values)
-        return list(map(lambda x : x/maxValue, self.values))
-
-    @property
-    def normalizedError(self):
-        maxValue = max(self.values)
-        return list(map(lambda x : x/maxValue, self.error))
-
-    def add(self, value):
-        if isinstance(value, Ray):
-            ray = value
-            index = self.whichBin(ray.y)
-        else:
-            index = self.whichBin(value)
-        self.values[index] += 1
-
-    def whichBin(self, value):
-        if value <= self.min:
-            return 0
-        elif value >= self.max:
-            return len(self.values)-1
-        
-        return int((value - self.min)/self.delta)
-
-    def show(self, title="Intensity profile", bins='auto', normalized=True):
-        plt.ioff()
-        plt.errorbar(self.range, self.normalizedValues, yerr=self.normalizedError, fmt="ko-")
-        plt.xlabel("Distance")
-        plt.ylabel("Intensity [arb.u]")
-        plt.title(title)
-        plt.show()
+        return Ray(y, theta, intensity)
+  
 
