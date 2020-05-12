440dc98584e6a105054b744dc713647c071f5724
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2020-05-12 12:22:08 -0400

    Fixed an issue with warning format

--- raytracing/tests/testsMatrix.py
+++ raytracing/tests/testsMatrix.py
@@ -1,5 +1,6 @@
 import unittest
-import env # modifies path
+import env  # modifies path
+
 from raytracing import *
 
 inf = float("+inf")
@@ -12,7 +13,7 @@ class TestMatrix(unittest.TestCase):
 
     def testMatrixExplicit(self):
         m = Matrix(A=1, B=2, C=3, D=4, physicalLength=1,
-            frontVertex=0, backVertex=0, apertureDiameter=1.0)
+                   frontVertex=0, backVertex=0, apertureDiameter=1.0)
         self.assertIsNotNone(m)
         self.assertEqual(m.A, 1)
         self.assertEqual(m.B, 2)
@@ -22,16 +23,16 @@ class TestMatrix(unittest.TestCase):
     def testMatrixProductMath(self):
         m1 = Matrix(A=1, B=2, C=3, D=4)
         m2 = Matrix(A=5, B=6, C=7, D=8)
-        m3 = m2 * m1        
-        self.assertEqual(m3.A, 1*5 + 3*6)
-        self.assertEqual(m3.B, 2*5 + 4*6)
-        self.assertEqual(m3.C, 1*7 + 3*8)
-        self.assertEqual(m3.D, 2*7 + 4*8)
+        m3 = m2 * m1
+        self.assertEqual(m3.A, 1 * 5 + 3 * 6)
+        self.assertEqual(m3.B, 2 * 5 + 4 * 6)
+        self.assertEqual(m3.C, 1 * 7 + 3 * 8)
+        self.assertEqual(m3.D, 2 * 7 + 4 * 8)
 
     def testMatrixProductLength(self):
         m1 = Matrix(A=1, B=2, C=3, D=4)
         m2 = Matrix(A=5, B=6, C=7, D=8)
-        m3 = m2 * m1        
+        m3 = m2 * m1
         self.assertEqual(m3.L, m1.L + m2.L)
         self.assertIsNone(m3.frontVertex)
         self.assertIsNone(m3.backVertex)
@@ -44,15 +45,15 @@ class TestMatrix(unittest.TestCase):
     def testMatrixProductVerticesAllNone(self):
         m1 = Matrix(A=1, B=2, C=3, D=4)
         m2 = Matrix(A=5, B=6, C=7, D=8)
-        m3 = m2 * m1        
+        m3 = m2 * m1
         self.assertEqual(m3.L, m1.L + m2.L)
         self.assertIsNone(m3.frontVertex)
         self.assertIsNone(m3.backVertex)
 
     def testMatrixProductVerticesSecondNone(self):
-        m1 = Matrix(A=1, B=2, C=3, D=4, physicalLength=10,frontVertex=0, backVertex=10)
+        m1 = Matrix(A=1, B=2, C=3, D=4, physicalLength=10, frontVertex=0, backVertex=10)
         m2 = Matrix(A=5, B=6, C=7, D=8)
-        m3 = m2 * m1        
+        m3 = m2 * m1
         self.assertEqual(m3.L, m1.L + m2.L)
         self.assertEqual(m3.frontVertex, 0)
         self.assertEqual(m3.backVertex, 10)
@@ -60,7 +61,7 @@ class TestMatrix(unittest.TestCase):
     def testMatrixProductVerticesFirstNone(self):
         m1 = Matrix(A=1, B=2, C=3, D=4)
         m2 = Matrix(A=5, B=6, C=7, D=8, physicalLength=10, frontVertex=0, backVertex=10)
-        m3 = m2 * m1        
+        m3 = m2 * m1
         self.assertEqual(m3.L, m1.L + m2.L)
         self.assertEqual(m3.frontVertex, 0)
         self.assertEqual(m3.backVertex, 10)
@@ -68,7 +69,7 @@ class TestMatrix(unittest.TestCase):
     def testMatrixProductVerticesTwoElements(self):
         m1 = Matrix(A=1, B=2, C=3, D=4, physicalLength=5, frontVertex=0, backVertex=5)
         m2 = Matrix(A=5, B=6, C=7, D=8, physicalLength=10, frontVertex=0, backVertex=10)
-        m3 = m2 * m1        
+        m3 = m2 * m1
         self.assertEqual(m3.L, m1.L + m2.L)
         self.assertEqual(m3.frontVertex, 0)
         self.assertEqual(m3.backVertex, 15)
@@ -76,12 +77,12 @@ class TestMatrix(unittest.TestCase):
     def testMatrixProductVerticesTwoElementsRepresentingGroups(self):
         m1 = Matrix(A=1, B=2, C=3, D=4, physicalLength=5, frontVertex=1, backVertex=4)
         m2 = Matrix(A=5, B=6, C=7, D=8, physicalLength=10, frontVertex=2, backVertex=9)
-        m3 = m2 * m1        
+        m3 = m2 * m1
         self.assertEqual(m3.L, m1.L + m2.L)
         self.assertEqual(m3.frontVertex, 1)
         self.assertEqual(m3.backVertex, 14)
 
-        m3 = m1 * m2        
+        m3 = m1 * m2
         self.assertEqual(m3.L, m1.L + m2.L)
         self.assertEqual(m3.frontVertex, 2)
         self.assertEqual(m3.backVertex, 14)
@@ -94,7 +95,7 @@ class TestMatrix(unittest.TestCase):
         self.assertFalse(m2.hasFiniteApertureDiameter())
         self.assertEqual(m2.largestDiameter(), float("+inf"))
 
-    def testTransferMatrix(self):   
+    def testTransferMatrix(self):
         m1 = Matrix(A=1, B=2, C=3, D=4)
         # Null length returns self
         self.assertEqual(m1.transferMatrix(), m1)
@@ -103,16 +104,17 @@ class TestMatrix(unittest.TestCase):
         m2 = Matrix(A=1, B=2, C=3, D=4, physicalLength=1)
         self.assertEqual(m2.transferMatrix(upTo=1), m2)
         self.assertEqual(m2.transferMatrix(upTo=2), m2)
-        
+
         # Length == 1 raises exception if upTo<1: can't do partial
         # Subclasses Space() and DielectricSlab() can handle this
         # (not the generic matrix).
         with self.assertRaises(Exception) as context:
             m2.transferMatrix(upTo=0.5)
 
-    def testTransferMatrices(self):   
+    def testTransferMatrices(self):
         m1 = Matrix(A=1, B=2, C=3, D=4)
         self.assertEqual(m1.transferMatrices(), [m1])
+        m1 * GaussianBeam(w=1, n=2)
 
     def testIsImaging(self):
         m1 = Matrix(A=1, B=0, C=3, D=4)
@@ -121,37 +123,37 @@ class TestMatrix(unittest.TestCase):
         self.assertFalse(m2.isImaging)
 
     def testFiniteForwardConjugate(self):
-        m1 = Lens(f=5)*Space(d=10)
-        (d,m2) = m1.forwardConjugate()
+        m1 = Lens(f=5) * Space(d=10)
+        (d, m2) = m1.forwardConjugate()
         self.assertTrue(m2.isImaging)
         self.assertEqual(d, 10)
         self.assertEqual(m1.determinant, 1)
         self.assertEqual(m2.determinant, 1)
 
-        m1 = Space(d=5)*Lens(f=5)*Space(d=10)
-        (d,m2) = m1.forwardConjugate()
+        m1 = Space(d=5) * Lens(f=5) * Space(d=10)
+        (d, m2) = m1.forwardConjugate()
         self.assertTrue(m2.isImaging)
         self.assertEqual(d, 5)
         self.assertEqual(m2.determinant, 1)
 
     def deactivated_testInfiniteForwardConjugate(self):
-        m1 = Lens(f=5)*Space(d=5)
-        (d,m2) = m1.forwardConjugate()
+        m1 = Lens(f=5) * Space(d=5)
+        (d, m2) = m1.forwardConjugate()
         self.assertTrue(m2.isImaging)
         self.assertEqual(d, float("+inf"))
         self.assertEqual(m1.determinant, 1)
         self.assertEqual(m2.determinant, 1)
 
     def testFiniteBackConjugate(self):
-        m1 = Space(d=10)*Lens(f=5)
-        (d,m2) = m1.backwardConjugate()
+        m1 = Space(d=10) * Lens(f=5)
+        (d, m2) = m1.backwardConjugate()
         self.assertTrue(m2.isImaging)
         self.assertEqual(d, 10)
         self.assertEqual(m1.determinant, 1)
         self.assertEqual(m2.determinant, 1)
 
-        m1 = Space(d=10)*Lens(f=5)*Space(d=5)
-        (d,m2) = m1.backwardConjugate()
+        m1 = Space(d=10) * Lens(f=5) * Space(d=5)
+        (d, m2) = m1.backwardConjugate()
         self.assertTrue(m2.isImaging)
         self.assertEqual(d, 5)
         self.assertEqual(m1.determinant, 1)
@@ -172,7 +174,7 @@ class TestMatrix(unittest.TestCase):
         self.assertIsNone(s.frontVertex)
         self.assertIsNone(s.backVertex)
 
-        s = Space(d=10)*Space(d=5)
+        s = Space(d=10) * Space(d=5)
         self.assertEqual(s.B, 15)
         self.assertEqual(s.L, 15)
         self.assertEqual(s.determinant, 1)
@@ -193,7 +195,7 @@ class TestMatrix(unittest.TestCase):
         # This should work, not sure how to deal
         # with this failed test: C is identically
         # zero and 0 * d->inf == 0 (I think).
-        s = Space(d=1)*Space(d=inf)
+        s = Space(d=1) * Space(d=inf)
         self.assertEqual(s.A, 1)
         self.assertEqual(s.B, inf)
         self.assertEqual(s.C, 0)
@@ -202,7 +204,7 @@ class TestMatrix(unittest.TestCase):
         self.assertIsNone(s.frontVertex)
         self.assertIsNone(s.backVertex)
 
-        s = Space(d=inf)*Space(d=1)
+        s = Space(d=inf) * Space(d=1)
         self.assertEqual(s.A, 1)
         self.assertEqual(s.B, inf)
         self.assertEqual(s.C, 0)
@@ -211,7 +213,7 @@ class TestMatrix(unittest.TestCase):
         self.assertIsNone(s.frontVertex)
         self.assertIsNone(s.backVertex)
 
-        s = Space(d=inf)*Space(d=inf)
+        s = Space(d=inf) * Space(d=inf)
         self.assertEqual(s.A, 1)
         self.assertEqual(s.B, inf)
         self.assertEqual(s.C, 0)
@@ -222,7 +224,7 @@ class TestMatrix(unittest.TestCase):
 
     def testLensMatrix(self):
         s = Lens(f=10)
-        self.assertEqual(s.C, -1/10)
+        self.assertEqual(s.C, -1 / 10)
         self.assertEqual(s.determinant, 1)
         self.assertEqual(s.frontVertex, 0)
         self.assertEqual(s.backVertex, 0)
@@ -240,13 +242,13 @@ class TestMatrix(unittest.TestCase):
 
     def testDielectricInterface(self):
         m = DielectricInterface(n1=1, n2=1.5, R=10)
-        self.assertEqual(m.determinant, 1/1.5)
+        self.assertEqual(m.determinant, 1 / 1.5)
         self.assertEqual(m.frontVertex, 0)
         self.assertEqual(m.backVertex, 0)
 
     def testLensFocalLengths(self):
         m = Lens(f=5)
-        self.assertEqual(m.effectiveFocalLengths(), (5,5))
+        self.assertEqual(m.effectiveFocalLengths(), (5, 5))
         self.assertEqual(m.backFocalLength(), 5)
         self.assertEqual(m.frontFocalLength(), 5)
 
@@ -294,9 +296,9 @@ class TestMatrix(unittest.TestCase):
         l = thorlabs.AC508_750_B()
         l = thorlabs.AC508_1000_B()
 
-
     def testEdmundLens(self):
         l = eo.PN_33_921()
+
 
 if __name__ == '__main__':
     unittest.main()
