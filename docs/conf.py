fe5c743e5b06fa73221c248b56deefb78884ac23
JLBegin <ludovick.begin@gmail.com>
2020-05-14 14:49:03 -0400

    add markdown support

--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,7 @@ author = 'DCC-Lab'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 'recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
