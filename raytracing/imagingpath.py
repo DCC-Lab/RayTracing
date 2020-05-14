7f4105140b77aa209809558ee441b807545ebc3f
JLBegin <ludovick.begin@gmail.com>
2020-05-14 08:56:52 -0400

    temporary Fixme annotation

--- raytracing/imagingpath.py
+++ raytracing/imagingpath.py
@@ -313,7 +313,7 @@ class ImagingPath(MatrixGroup):
 
         if not self.hasFiniteApertureDiameter() and axes is not None:
             if len(axes.lines) != 0:
-                for rayTrace in axes.lines:
+                for rayTrace in axes.lines:  # FIXME: need a more robust reference to rayTraces
                     if max(abs(rayTrace._y)) * 2 > displayRange:
                         displayRange = max(abs(rayTrace._y)) * 2
 
