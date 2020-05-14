6fed563607acbea2c8a4ad1f48ad4fef25ca991d
Marc <marc-andre.vigneault.02@hotmail.com>
2020-05-14 12:58:10 -0400

    highlight section for code pygments and create html file

--- raytracing/__main__.py
+++ raytracing/__main__.py
@@ -1,50 +1,65 @@
-from .imagingpath import *
-from .laserpath import *
-
-from .specialtylenses import *
-from .axicon import *
-import raytracing.thorlabs as thorlabs
-import raytracing.eo as eo
-import raytracing.olympus as olympus
 import argparse
+from pygments import highlight
+import pygments.lexers
+import pygments.formatters
+import webbrowser
+import os
 
 
 ap = argparse.ArgumentParser(prog='python -m raytracing')
-ap.add_argument("-e", "--examples", required=False, default='all', help="Specific example numbers, separated by a comma")
+ap.add_argument("-e", "--examples", required=False, default='', help="Specific example numbers, separated by a comma")
 
 args = vars(ap.parse_args())
 examples = args['examples']
-
-if examples == 'all':
-    examples = range(1,30)
-else:
-    examples = [ int(y) for y in examples.split(',')]
 
 
 class ExampleManager:
     def __init__(self, arguments):
         self.arguments = arguments
         self.exampleDirPath = os.path.dirname(os.path.realpath(__file__)) + "\..\examples\\argsExamples"
+        self.outputExampleFilePath = self.exampleDirPath + "\exampleFile.html"
+        self.selectedFile = None
+        self.formatter = pygments.formatters.get_formatter_by_name('html',full=True, linenos=True, cssclass="source", style='default')
+        self.lexer = pygments.lexers.get_lexer_by_name('python', stripall=True)
         self.parseArguments()
 
-    def exampleCarousel(self):
-        pass
+    def exampleCarousel(self, exampleIndexList):
+        for i in exampleIndexList:
+            self.selectedFile = self.exampleDirPath + '\example{}'.format(i)
+            self.showExample()
 
     def showExample(self):
-        pass
+        self.generateExample()
+        webbrowser.open(self.outputExampleFilePath)
 
-    def generatePdfExample(self):
+    def generateExample(self):
+        self.generateFigureFromCode()
+        self.generateHTMLHighlightedCode()
+        self.mergeFigureAndHTML()
+        # OPEN HTML FILE.
+
+    def mergeFigureAndHTML(self):
         pass
 
     def generateFigureFromCode(self):
         pass
 
-    def generateHighlightedCodePdf(self):
-        pass
+    def generateHTMLHighlightedCode(self, code):
+        with open(self.outputExampleFilePath, 'w') as f:
+            highlightedCode = highlight(code, self.lexer, self.formatter, outfile=f)
+            f.close()
+        return highlightedCode
 
-    def highlightCode(self):
+    def getExampleCode(self):
         pass
 
     def parseArguments(self):
-        pass
+        print(self.arguments)
+        if 'examples' in self.arguments:
+            print(list(self.arguments['examples']))
+            self.exampleCarousel(list(self.arguments['examples']))
+
+
+manager = ExampleManager(args)
+manager.generateHTMLHighlightedCode("print ('Hello World')\nclass Hello:\n\tdef __init__(self):\n\tpass")
 
