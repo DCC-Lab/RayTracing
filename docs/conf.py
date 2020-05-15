a170f2df41fa6e47d923ad889257ee32e4104c57
JLBegin <ludovick.begin@gmail.com>
2020-05-15 17:01:13 -0400

    Update conf.py
    add theme path to help RTD find the theme

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
@@ -41,8 +42,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
