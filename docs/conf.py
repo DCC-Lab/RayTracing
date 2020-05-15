00c012b2ccc483a8d385565ddc56f7f39d74c56d
JLBegin <ludovick.begin@gmail.com>
2020-05-15 17:08:21 -0400

    Update conf.py
    append extensions with html theme

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'DCC-Lab'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.napoleon', 'recommonmark']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.napoleon', 'recommonmark', "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
