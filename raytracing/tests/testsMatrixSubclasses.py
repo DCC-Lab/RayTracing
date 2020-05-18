9561cceeebf8b4107b3298cfb1ad600388125010
Daniel Côté <dccote@novajo.ca>
2020-05-18 11:55:20 -0400

    Deleted redundant tests files and included  a test that was missing before

--- raytracing/tests/testsMatrixSubclasses.py
+++ raytracing/tests/testsMatrixSubclasses.py
@@ -131,7 +131,6 @@ class TestCurvedMirror(unittest.TestCase):
         self.assertAlmostEqual(m1.C, m2.C, 4)
         self.assertAlmostEqual(m1.D, m2.D, 4)
 
-
 class TestSpaceMatrix(unittest.TestCase):
 
     def testSpaceMatrix(self):
