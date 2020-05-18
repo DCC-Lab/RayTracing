d6d23ab91e14eee54ccd7b73003b070fcc5610a5
Daniel Côté <dccote@novajo.ca>
2020-05-18 11:39:36 -0400

    Some change were merged incorrectly by dccote.

--- raytracing/tests/testsThickLens.py
+++ raytracing/tests/testsThickLens.py
@@ -36,16 +36,6 @@ class TestThickLens(unittest.TestCase):
         m = ThickLens(R1=-10, R2 = 20, n=1.5, thickness=1)
         self.assertIsNotNone(m)
 
-    def testRayConvergingThickLens(self):
-        m1 = ThickLens(R1=-10, R2 = 20, n=1.5, thickness=1)
-        outRay1 = m1*Ray(y=1,theta=0)
-        self.assertTrue(outRay1.theta < 0)
-
-    def testRayDivergingThickLens(self):
-        m2 = ThickLens(R1=10, R2 = -20, n=1.5, thickness=1)
-        outRay2 = m2*Ray(y=1,theta=0)
-        self.assertTrue(outRay2.theta >  0)
-
     def testRayInFlippedThickLens(self):
         m1 = CurvedMirror(R=-10)
         outRay1 = m1*Ray(y=1,theta=0)
