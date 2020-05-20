5f7ea6b406d1e2fe8f80098e9d45c3611f5fb1d6
Daniel Côté <dccote@novajo.ca>
2020-05-20 16:54:55 -0400

    Update testsLimits.py
    new limits and tests

--- raytracing/tests/testsLimits.py
+++ raytracing/tests/testsLimits.py
@@ -5,7 +5,7 @@ from raytracing import *
 inf = float("+inf")
 
 class TestLimits(unittest.TestCase):
-    def testInfinityAndZero(self):
+    def testLimitsThatExist(self):
         # Limits that exist
         self.assertEqual(inf, float('+inf'))
         self.assertEqual(0/inf, 0)
@@ -15,14 +15,18 @@ class TestLimits(unittest.TestCase):
         self.assertEqual(inf+10, inf)
         self.assertEqual(inf*(-inf), -inf)
         self.assertEqual((-inf)*(-inf), inf)
-
-        # Limits that do not exist
-        self.assertTrue(isnan(inf-inf))
-        self.assertTrue(isnan(inf*0))
+        self.assertEqual((inf)**(inf), inf)
 
         # Dividing by zero is never allowed
         with self.assertRaises(Exception) as context:
             inf/0
+
+    def testLimitsThatDontExist(self):
+        self.assertTrue(isnan(inf-inf))
+        self.assertTrue(isnan(inf*0))
+
+    def testLimitsThatExistButPythonDoesNotConsider(self):
+        self.assertEqual((1.0)**(inf), 1)
 
 
 
