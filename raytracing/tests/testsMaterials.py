685f09092d5e55db3f580bdb62fd4fa54cde18e2
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2020-05-20 12:03:26 -0400

    TestN_BK7

--- raytracing/tests/testsMaterials.py
+++ raytracing/tests/testsMaterials.py
@@ -7,10 +7,19 @@ class TestMaterial(unittest.TestCase):
     def testMaterialInvalid(self):
         self.assertRaises(TypeError, Material.n, 5)
 
-class TestN_BK7(unittest.TestCase):
-    def testN_BK7Invalid(self):
-        
 
+class TestN_BK7(unittest.TestCase):
+    def testN_BK7TypeErrors(self):
+        self.assertRaises(TypeError, N_BK7.n, None)
+        self.assertRaises(TypeError, N_BK7.n, 'test')
+
+    def testN_BK7ValueErrors(self):
+        self.assertRaises(ValueError, N_BK7.n, 100)
+        self.assertRaises(ValueError, N_BK7.n, 0)
+        self.assertRaises(ValueError, N_BK7.n, -100)
+
+    def testN_BK7(self):
+        self.assertEqual(N_BK7.n(5), 1.3965252243506636)
 
 
 if __name__ == '__main__':
