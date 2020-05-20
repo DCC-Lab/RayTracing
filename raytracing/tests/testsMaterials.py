07f22eb392cbc14661c3c0140fad3698e9ee4ff8
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2020-05-20 14:08:58 -0400

    TestN_LASF9.

--- raytracing/tests/testsMaterials.py
+++ raytracing/tests/testsMaterials.py
@@ -205,7 +205,17 @@ class TestLAFN7(unittest.TestCase):
 
 
 class TestN_LASF9(unittest.TestCase):
-    pass
+    def testN_LASF9TypeErrors(self):
+        self.assertRaises(TypeError, N_LASF9.n, None)
+        self.assertRaises(TypeError, N_LASF9.n, 'test')
+
+    def testN_LASF9ValueErrors(self):
+        self.assertRaises(ValueError, N_LASF9.n, 100)
+        self.assertRaises(ValueError, N_LASF9.n, 0)
+        self.assertRaises(ValueError, N_LASF9.n, -100)
+
+    def testN_LASF9(self):
+        self.assertEqual(1.7197092324835102, N_LASF9.n(5))
 
 
 class TestN_LAK22(unittest.TestCase):
