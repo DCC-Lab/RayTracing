bbb824c088828b54402d43d3c9939d99766429d4
Valérie <valerie.pineau-noel.1@ulaval.ca>
2020-05-21 12:27:33 -0400

    changed positions in nm instead of mm and added some comments

--- examples/confocalDetection.py
+++ examples/confocalDetection.py
@@ -1,22 +1,23 @@
 from raytracing import *
 import matplotlib.pyplot as plt
 
-pinholeModifier = {1/3:[], 1:[], 3:[]}
-
+pinholeModifier = {1/3:[], 1:[], 3:[]} # Dictionary having a pinhole factor with an empty list which will subsequently contain the
+                                       # transmission efficiency for each focal spot position
 for pinhole in pinholeModifier:
 
     finalRays = pinholeModifier[pinhole]
-    pinholeSize = 0.009374*pinhole
-    positions = [5.001000, 5.000800, 5.000500, 5.000300, 5.000150, 5.000100, 5.000050, 5.000025, 5, 4.999975, 4.999950, 4.999900, 4.999850, 4.999700, 4.999500, 4.999200, 4.999000]
-    print('.')
+    pinholeSize = 0.009374*pinhole # Ideal size of the pinhole times a certain factor
+    positions = [1000, 800, 500, 300, 150, 100, 50, 25, 0, -25, -50, -100, -150, -300, -500, -800, -1000] # list of all relative positions from the ideal focal spot position in nm
+    print(".")
 
     for z in positions:
 
-        nRays = 10000  # Number of total rays produced by the source
-        inputRays = RandomUniformRays(yMax=0.000250, yMin=-0.000250, maxCount=nRays)
-
+        newPositions = 5 + (z*0.000001) # working distance of the objective + position
+        nRays = 10000  # Number of total rays produced by the focal spot
+        inputRays = RandomUniformRays(yMax=0.000250, yMin=-0.000250, maxCount=nRays) # Production of rays from a focal spot of 250 nm
+                                                                                     # of radius
         illumination = ImagingPath()
-        illumination.append(Space(d=z))
+        illumination.append(Space(d=newPositions))
         illumination.append(Lens(f=5))
         illumination.append(Space(d=105))
         illumination.append(Lens(f=100))
@@ -31,15 +32,15 @@ for pinhole in pinholeModifier:
         illumination.append(Space(d=50))
         illumination.append(Aperture(diameter=pinholeSize))
 
-        outputRays = illumination.traceManyThrough(inputRays, progress=False)
-        finalRays.append(outputRays.count/inputRays.count)
+        outputRays = illumination.traceManyThrough(inputRays, progress=False) # Counts how many rays make it through the pinhole
+        finalRays.append(outputRays.count/inputRays.count) # Calculates the transmission efficiency
 
-    pinholeModifier[pinhole] = finalRays
+    pinholeModifier[pinhole] = finalRays # Incorporates the final list of transmission efficiencies into the dictionary
 
 plt.plot(positions, pinholeModifier[1/3], label='Pinhole Size S/3')
 plt.plot(positions, pinholeModifier[1], label='Pinhole Size S')
 plt.plot(positions, pinholeModifier[3], label='Pinhole Size 3S')
 plt.ylabel('Transmission efficiency')
-plt.xlabel('Position of the focal spot (mm)')
+plt.xlabel('Position of the focal spot (nm)')
 plt.legend()
 plt.show()
