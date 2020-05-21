11256f5d3a5c50994e13e8eeb0b0be72d574ee8e
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2020-05-21 16:19:44 -0400

    New tests

--- raytracing/tests/testsRaysSubclasses.py
+++ raytracing/tests/testsRaysSubclasses.py
@@ -44,23 +44,86 @@ class TestUniformRays(unittest.TestCase):
 
 class TestLambertianRays(unittest.TestCase):
 
+    @unittest.skip("thetaMax and thetaMin are swapped. Should be fixed soon.")
     def testLambertianRays(self):
         rays = LambertianRays(10, 0, 10, 10, 10)
         self.assertIsNotNone(rays)
         self.assertEqual(rays.yMax, 10)
         self.assertEqual(rays.yMin, 0)
-        self.assertEqual(rays.thetaMax, -pi / 2)
-        self.assertEqual(rays.thetaMin, -(-pi / 2))
+        self.assertEqual(rays.thetaMax, pi / 2)
+        self.assertEqual(rays.thetaMin, -pi / 2)
         self.assertEqual(rays.M, 10)
         self.assertEqual(rays.N, 10)
         self.assertEqual(rays.I, 10)
         allRays = []
-        for theta in linspace(pi / 2, -pi / 2, 10):
+        for theta in linspace(-pi / 2, pi / 2, 10):
             intensity = int(10 * cos(theta))
             for y in linspace(0, 10, 10):
                 for _ in range(intensity):
                     allRays.append(Ray(y, theta))
         self.assertListEqual(rays.rays, allRays)
+
+    @unittest.skip("thetaMax and thetaMin are swapped. Should be fixed soon.")
+    def testLambertianRaysNoneInArgs(self):
+        rays = LambertianRays(10, None, 10, 10, 10)
+        self.assertIsNotNone(rays)
+        self.assertEqual(rays.yMax, 10)
+        self.assertEqual(rays.yMin, -10)
+        self.assertEqual(rays.thetaMax, pi / 2)
+        self.assertEqual(rays.thetaMin, -pi / 2)
+        self.assertEqual(rays.M, 10)
+        self.assertEqual(rays.N, 10)
+        self.assertEqual(rays.I, 10)
+        allRays = []
+        for theta in linspace(-pi / 2, pi / 2, 10):
+            intensity = int(10 * cos(theta))
+            for y in linspace(0, 10, 10):
+                for _ in range(intensity):
+                    allRays.append(Ray(y, theta))
+        self.assertListEqual(rays.rays, allRays)
+
+
+class TestRandomRays(unittest.TestCase):
+
+    def testRandomRays(self):
+        rays = RandomRays(10, 0, pi / 3, 0, 100)
+        self.assertIsNotNone(rays)
+        self.assertEqual(rays.maxCount, 100)
+        self.assertEqual(rays.yMax, 10)
+        self.assertEqual(rays.yMin, 0)
+        self.assertEqual(rays.thetaMax, pi / 3)
+        self.assertEqual(rays.thetaMin, 0)
+
+    def testRandomRaysWithNoneInArgs(self):
+        rays = RandomRays(10, None, pi / 2, None, 1000)
+        self.assertIsNotNone(rays)
+        self.assertEqual(rays.maxCount, 1000)
+        self.assertEqual(rays.yMax, 10)
+        self.assertEqual(rays.yMin, -10)
+        self.assertEqual(rays.thetaMax, pi / 2)
+        self.assertEqual(rays.thetaMin, -pi / 2)
+
+    @unittest.skip("Should be fixed soon.")
+    def testRandomRay(self):
+        rays = RandomRays()
+        with self.assertRaises(NotImplementedError):
+            rays.randomRay()
+
+    def testLen(self):
+        rays = RandomRays(maxCount=10)
+        self.assertEqual(len(rays), 10)
+
+    @unittest.skip("Should be fixed soon.")
+    def testNext(self):
+        rays = RandomRays()
+        with self.assertRaises(NotImplementedError):
+            next(rays)
+
+    def testNextOutOfBound(self):
+        rays = RandomRays(maxCount=1)
+        rays.iteration = 1
+        with self.assertRaises(StopIteration):
+            next(rays)
 
 
 if __name__ == '__main__':
