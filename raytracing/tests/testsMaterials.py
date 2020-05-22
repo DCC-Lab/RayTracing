579222e876fe89dd7d94414ca69cf61a1a88d6d0
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2020-05-22 08:23:03 -0400

    Automation : Refactoring names.

--- raytracing/tests/testsMaterials.py
+++ raytracing/tests/testsMaterials.py
@@ -14,27 +14,27 @@ class TestMaterialSubclasses(unittest.TestCase):
         self.fails = []
 
         for material in Material.__subclasses__():
-            self.subclasses.append(material)
+            self.materials.append(material)
 
     def tearDown(self):
         self.assertEqual([], self.fails)
 
     def testMaterialSubclassesTypeError(self):
-        for subclass in self.subclasses:
+        for material in self.materials:
             try:
-                self.assertRaises(TypeError, subclass.n, None)
-                self.assertRaises(TypeError, subclass.n, 'test')
+                self.assertRaises(TypeError, material.n, None)
+                self.assertRaises(TypeError, material.n, 'test')
             except AssertionError:
-                self.fails.append('TypeError for subclass {}'.format(subclass.__name__))
+                self.fails.append('TypeError for subclass {}'.format(material.__name__))
 
     def testMaterialSubclassesValueErrors(self):
-        for subclass in self.subclasses:
+        for material in self.materials:
             try:
-                self.assertRaises(ValueError, subclass.n, 100)
-                self.assertRaises(ValueError, subclass.n, 0)
-                self.assertRaises(ValueError, subclass.n, -100)
+                self.assertRaises(ValueError, material.n, 100)
+                self.assertRaises(ValueError, material.n, 0)
+                self.assertRaises(ValueError, material.n, -100)
             except AssertionError:
-                self.fails.append('ValueError for subclass {}'.format(subclass.__name__))
+                self.fails.append('ValueError for subclass {}'.format(material.__name__))
 
     def testMaterialSubclasses(self):
         ''' These are sample values of refractive indexes for each subclass of material for a wavelength of 5 micron.
@@ -45,10 +45,10 @@ class TestMaterialSubclasses(unittest.TestCase):
                              1.4251526811342752, 1.5810761652641074, 1.7197092324835102, 1.528065402814606,
                              1.541289278092723, 1.5975207997018837, 1.3404572894914368]
 
-        for subclass in self.subclasses:
-            n = subclass.n(5)
+        for material in self.materials:
+            n = material.n(5)
             if n not in refractiveIndexes:
-                self.fails.append('Wrong value for subclass {}, {} not a valid n value.'.format(subclass.__name__, str(n)))
+                self.fails.append('Wrong value for subclass {}, {} not a valid n value.'.format(material.__name__, str(n)))
 
 
 if __name__ == '__main__':
