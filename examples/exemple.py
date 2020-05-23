5b5244c5f64c206c6f490af00dbf5ef31008550b
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-05-23 16:11:42 -0400

    Update exemple.py
    update

--- examples/exemple.py
+++ examples/exemple.py
@@ -35,23 +35,5 @@ path.append(Space(d=100))
 path.append(Lens(f=100,diameter=25))
 path.append(Space(d=100))
 
-'''
-plt.figure(1)
 
-#1
-plt.subplot2grid((2,2), (0,0))
-plt.title('..')
-
-#2
-plt.subplot2grid((2,2), (0,1))
-plt.title('..')
-
-#3
-plt.subplot2grid((2,2), (1,0), colspan = 2)
-plt.title('..')
-
-
-plt.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.3,
-                    wspace=0.35)
-plt.show()
 
