6aa7b08d4b7c9d081d7efd721a9b59d683f86abf
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-05-23 16:35:55 -0400

    exemple.py

---
+++ examples/exemple.py
@@ -0,0 +1,15 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath('../'))
+
+from raytracing import *
+
+path = ImagingPath()
+path.name = "Telescope"
+path.append(Space(d=50))
+path.append(Lens(f=50,diameter=25))
+path.append(Space(d=50))
+path.append(Space(d=100))
+path.append(Lens(f=100,diameter=25))
+path.append(Space(d=100))
+path.display()
