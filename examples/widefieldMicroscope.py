2ad16d4368d48c2d3c15454e1a0b61f7eb83abb7
Valérie <valerie.pineau-noel.1@ulaval.ca>
2020-05-23 16:31:16 -0400

    Change name to "widefieldMicroscope.py" + deleted old widefieldMicroscope.py file

--- examples/widefieldMicroscope.py
+++ examples/widefieldMicroscope.py
@@ -1,52 +1,17 @@
-'''
-DESCRIPTION
-
-
-When the lenses in a 4f system are too small, we get vignetting. We show a simple imaging system with vignetting, Field Stop poorly placed at a lens instead of the image with fix in next figure.
-Simple 4f system with lenses too small, shows vignetting 4f system with bigger lenses, show no more vignetting and good image size
-
-'''
+import sys
+import os
+sys.path.insert(0, os.path.abspath('../'))
 
 from raytracing import *
 
 
 
 path = ImagingPath()
-path.objectHeight = 15
-path.append(Space(d=5))
-path.append(Lens(f=5, diameter=25.4))
-path.append(Space(d=10))
-path.append(Lens(f=5, diameter=20))
-path.append(Space(d=5))
-path.append(Aperture(diameter=10,label='Camera'))
-path.append(Space(d=5))
-
-# print(path.imageSize())
-path.display()
-
-
-path = ImagingPath()
-path.objectHeight = 15
-path.append(Space(d=5))
-path.append(Lens(f=5, diameter=25.4))
-path.append(Space(d=10))
-path.append(Lens(f=5, diameter=25.4))
-path.append(Space(d=5))
-path.append(Aperture(diameter=10,label='Camera'))
-path.append(Space(d=5))
-
-# print(path.imageSize())
-path.display()
-
-path = ImagingPath()
-path.objectHeight = 15
-path.append(Space(d=5))
-path.append(Lens(f=5, diameter=20))
-path.append(Space(d=10))
-path.append(Lens(f=5, diameter=20))
-path.append(Space(d=5))
-path.append(Aperture(diameter=10,label='Camera'))
-path.append(Space(d=5))
-
-# print(path.imageSize())
+path.name = "Telescope"
+path.append(Space(d=50))
+path.append(Lens(f=50,diameter=25))
+path.append(Space(d=50))
+path.append(Space(d=100))
+path.append(Lens(f=100,diameter=25))
+path.append(Space(d=100))
 path.display()
