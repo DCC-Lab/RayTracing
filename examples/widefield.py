bafbfd9c5bd5c0ae7d695f28b2862977e1019fd6
Valérie <valerie.pineau-noel.1@ulaval.ca>
2020-05-24 11:47:14 -0400

    widefield.py test different lens diameter

--- examples/widefield.py
+++ examples/widefield.py
@@ -12,6 +12,7 @@ The following code shows a simple imaging system with three different paths cont
 '''
 
 path = ImagingPath()
+path.fanNumber = 3
 path.objectHeight = 10
 path.append(Space(d=50))
 path.append(Lens(f=50,diameter=12))
@@ -26,6 +27,7 @@ path.display()
 
 path = ImagingPath()
 path.objectHeight = 10
+path.fanNumber = 3
 path.append(Space(d=50))
 path.append(Lens(f=50,diameter=40))
 path.append(Space(d=100))
@@ -39,6 +41,7 @@ path.display()
 
 path = ImagingPath()
 path.objectHeight = 10
+path.fanNumber = 3
 path.append(Space(d=50))
 path.append(Lens(f=50,diameter=40))
 path.append(Space(d=100))
