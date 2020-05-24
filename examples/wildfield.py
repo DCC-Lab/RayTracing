dfee5a55f0ca57ecedb1df1927f99710f7caba94
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-05-23 21:39:01 -0400

    Update wildfield.py
    final update

--- examples/wildfield.py
+++ examples/wildfield.py
@@ -1,13 +1,13 @@
 from raytracing import *
 
 '''
-In any imaging systems, lenses diameter are of great importance as they calculate Field Stop and Aperture Stop. It is AS that controls light acceptance and poorly placed FS causes vignetting and it has an impact on image quality.
+In any imaging systems, lens diameter is of great importance as it calculates Field Stop and Aperture Stop. It is AS that controls light acceptance and poorly placed FS causes vignetting and it has an impact on image quality.
 
 The following code shows a simple imaging system with three different paths containing different lens diameter.
 
 1. The first one shows that if both lenses are too small, the AS is the first lens and the FS is the second lens. We get  vignetting, since Field Stop is poorly placed at a lens instead of the image
 2. The Second one shows that the second lens is smaller than the first one, so the AS is on the second lens, FS as the camera.
-3. The last one shows that both lenses are big enough to make the first lens the AS and the “camera” as FS.
+3. The last one shows that both lenses are large enough to make the first lens the AS and the “camera” as FS.
 '''
 
 path = ImagingPath()
