95267d3d0e02011ba445b11d232a8d0a7117829f
Daniel Côté <dccote@novajo.ca>
2020-05-25 06:48:15 -0400

    Minor update
    Fixed up the language a bit. Added labels to lenses and a title describing the situation on each figure.

--- examples/widefield.py
+++ examples/widefield.py
@@ -1,55 +1,56 @@
 from raytracing import *
 
 '''
-In any imaging system, lens diameter is of great importance as it calculates Field Stop(FS) and Aperture Stop(AS). 
-It is AS that controls light acceptance and poorly placed FS causes vignetting and it has an impact on image quality.
+In any imaging system, lens diameters are of great importance as
+they dictate the position of the Aperture Stop(AS) and Field Stop(FS). 
+We recall that the AS controls the light acceptance and that a 
+poorly placed FS causes vignetting.
 
-The following code shows a simple imaging system with three different paths containing different lens diameter.
+The following code shows a simple imaging system with three different
+paths containing different lens diameter.
 
-(a). The first one shows that if both lenses are too small, the AS is the first lens and the FS is the second lens. 
-    We get vignetting on the final image, since the FS is poorly placed at a lens instead of the detector's camera. 
-(b). The second one shows that the second lens is smaller than the first one, so the AS is on the second lens, 
-    FS as the camera.
-(c). The last one shows that both lenses are big enough to make the first lens the AS and the detector's camera the FS.
+(a). The first one shows that if both lenses are too small, 
+     the AS is the first lens and the FS is the second lens.
+     We get vignetting on the final image, since the FS is poorly 
+     placed at a lens instead of the detector's camera. 
+(b). The second one shows that the second lens is smaller 
+     than the first one, so the FS is at the camera but we still have 
+     AS is on the second lens, which is suboptimal.
+(c). The last one shows that both lenses are big enough to make the first 
+     lens the AS and the detector's camera the FS.
 '''
 
 path = ImagingPath()
-path.fanNumber = 3
-path.objectHeight = 10
-path.label="(a)"
+path.label="(a) Vignetting with Field Stop poorly placed because of second lens diameter"
 path.append(Space(d=50))
-path.append(Lens(f=50, diameter=15))
+path.append(Lens(f=50, diameter=15, label="First lens"))
 path.append(Space(d=100))
-path.append(Lens(f=50, diameter=15))
+path.append(Lens(f=50, diameter=15, label="Second lens"))
 path.append(Space(d=50))
 path.append(Aperture(diameter=10, label='Camera'))
 path.append(Space(d=50))
-path.display()
+path.display(limitObjectToFieldOfView=True,onlyChiefAndMarginalRays=True)
 
 
 path = ImagingPath()
-path.objectHeight = 10
-path.fanNumber = 3
-path.label="(b)"
+path.label="(a) Suboptimal Aperture Stop at second lens, but without vignetting"
 path.append(Space(d=50))
-path.append(Lens(f=50, diameter=40))
+path.append(Lens(f=50, diameter=40, label="First lens"))
 path.append(Space(d=100))
-path.append(Lens(f=50, diameter=15))
+path.append(Lens(f=50, diameter=15, label="Second lens"))
 path.append(Space(d=50))
 path.append(Aperture(diameter=10, label='Camera'))
 path.append(Space(d=50))
-path.display()
+path.display(limitObjectToFieldOfView=True,onlyChiefAndMarginalRays=True)
 
 
 path = ImagingPath()
-path.objectHeight = 10
-path.fanNumber = 3
-path.label="(c)"
+path.label="(c) Optimal Aperture Stop at first lens and Field Stop at Camera"
 path.append(Space(d=50))
-path.append(Lens(f=50, diameter=25))
+path.append(Lens(f=50, diameter=25, label="First lens"))
 path.append(Space(d=100))
-path.append(Lens(f=50, diameter=25))
+path.append(Lens(f=50, diameter=25, label="Second lens"))
 path.append(Space(d=50))
 path.append(Aperture(diameter=10, label='Camera'))
 path.append(Space(d=50))
-path.display()
+path.display(limitObjectToFieldOfView=True,onlyChiefAndMarginalRays=True)
