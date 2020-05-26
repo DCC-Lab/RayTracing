3c2f807a2c6d3b10de478483ab4caf773bb5d634
Daniel Côté <dccote@novajo.ca>
2020-05-26 12:38:27 -0400

    Added newlines to warning messages
    According to documentation, we should terminate a warning with a newline.
    https://docs.python.org/3/library/warnings.html

--- raytracing/matrixgroup.py
+++ raytracing/matrixgroup.py
@@ -37,12 +37,12 @@ class MatrixGroup(Matrix):
             lastElement = self.elements[-1]
             if lastElement.backIndex != matrix.frontIndex:
                 if isinstance(matrix, Space): # For Space(), we fix it
-                    msg = "Fixing mismatched indices between last element and appended Space(). Use Space(d=someDistance, n=someIndex)."
+                    msg = "Fixing mismatched indices between last element and appended Space(). Use Space(d=someDistance, n=someIndex).\n"
                     warnings.warn(msg, UserWarning)
                     matrix.frontIndex = lastElement.backIndex
                     matrix.backIndex = matrix.frontIndex
                 else:
-                    msg = "Mismatch of indices between last element and appended element"
+                    msg = "Mismatch of indices between last element and appended element\n"
                     raise ValueError(msg)
 
         self.elements.append(matrix)
