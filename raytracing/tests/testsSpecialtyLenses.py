9def229889beca5e707f0d65b480569ff8b91459
MFournierQC <50327179+MFournierQC@users.noreply.github.com>
2020-05-29 16:49:52 -0400

    Proper tests and assertions.

--- raytracing/tests/testsSpecialtyLenses.py
+++ raytracing/tests/testsSpecialtyLenses.py
@@ -6,44 +6,113 @@ from raytracing import *
 
 
 class TestAchromatDoubletLens(unittest.TestCase):
-    @property
-    def attributes(self):
-        return None
+    def testInit(self):
+        pass
 
+
+class TestAchromatDoubletLensSubclasses(unittest.TestCase):
     def setUp(self) -> None:
         self.subclasses = AchromatDoubletLens.__subclasses__()
 
-    def tearDown(self) -> None:
-        pass
-
-    def testInit(self):
+    def testSubclassesInit(self):
+        fails = []
         for subclass in self.subclasses:
             achromat = subclass()
-            self.assertIsNotNone(achromat)
-            '''
-            for element in achromat.elements:
-                attributes = [achromat.fa, achromat.fb, achromat.R1, achromat.R2, achromat.R3, achromat.tc1,
-                              achromat.tc2, achromat.te, achromat.n1, achromat.n2, achromat.mat1, achromat.mat2,
-                              achromat.url]
-                print(attributes)
-            '''
 
-    def testDrawAt(self):
-        axes = plt.axes()
-        for subclass in self.subclasses:
-            achromat = subclass()
-            achromat.drawAt(1, axes)
-
-    def testDrawAperture(self):
-        axes = plt.axes()
-        for subclass in self.subclasses:
-            achromat = subclass()
-            achromat.drawAperture(1, axes)
+            try:
+                self.assertIsNotNone(achromat)
+            except AssertionError:
+                fails.append('{} not properly initiated.'.format(subclass.__name__))
+        self.assertEqual([], fails)
 
     def testPointsOfInterest(self):
+        fails = []
+        z = 0
         for subclass in self.subclasses:
             achromat = subclass()
-            poi = achromat.pointsOfInterest(1)
+            points = achromat.pointsOfInterest(z)
+
+            f = -1.0 / achromat.C
+            p1 = z - (1 - achromat.D) / achromat.C
+            ff = p1 - f
+
+            p2 = z + achromat.L + (1 - achromat.A) / achromat.C
+            fb = p2 + f
+
+            try:
+                self.assertAlmostEqual(points[0]['z'], ff)
+                self.assertAlmostEqual(points[1]['z'], fb)
+            except AssertionError:
+                fails.append('{} has the wrong points of interest.'.format(subclass.__name__))
+        self.assertEqual([], fails)
+
+
+class TestObjectives(unittest.TestCase):
+    def testInit(self):
+        pass
+
+
+class TestObjectivesSubclasses(unittest.TestCase):
+    def setUp(self) -> None:
+        self.subclasses = Objective.__subclasses__()
+
+    def testSubclassesInit(self):
+        fails = []
+        for subclass in self.subclasses:
+            objective = subclass()
+
+            try:
+                self.assertIsNotNone(objective)
+            except AssertionError:
+                fails.append('{} not properly initiated.'.format(subclass.__name__))
+        self.assertEqual([], fails)
+
+    def testFlipOrientation(self):
+        fails = []
+        for subclass in self.subclasses:
+            original = subclass()
+            flipped = subclass().flipOrientation()
+
+            try:
+                self.assertFalse(original.isFlipped)
+                self.assertTrue(flipped.isFlipped)
+                self.assertNotEqual(original.frontVertex, flipped.frontVertex)
+                self.assertNotEqual(original.backVertex, flipped.backVertex)
+            except AssertionError:
+                fails.append('{} was not properly flipped.'.format(subclass.__name__))
+        self.assertEqual([], fails)
+
+    def testPointsOfInterest(self):
+        fails = []
+        z = 10
+        for subclass in self.subclasses:
+            objective = subclass()
+            points = objective.pointsOfInterest(z)
+            ff = z + objective.focusToFocusLength
+            fb = z
+
+            try:
+                self.assertAlmostEqual(points[0]['z'], fb)
+                self.assertAlmostEqual(points[1]['z'], ff)
+            except AssertionError:
+                fails.append('{} has the wrong point of interest.'.format(subclass.__name__))
+        self.assertEqual([], fails)
+
+    def testPointsOfInterestFlipped(self):
+        fails = []
+        z = 10
+        for subclass in self.subclasses:
+            objective = subclass()
+            points = objective.pointsOfInterest(z)
+            ff = z + objective.focusToFocusLength
+            fb = z
+
+            try:
+                self.assertAlmostEqual(points[0]['z'], fb)
+                self.assertAlmostEqual(points[1]['z'], ff)
+            except AssertionError:
+                fails.append('{} has the wrong point of interest.'.format(subclass.__name__))
+        self.assertEqual([], fails)
 
 
 if __name__ == '__main__':
