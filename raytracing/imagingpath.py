66c2d2d7dd128eb5fb7cf9c92b642984a6d6a3ce
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2020-05-29 10:31:02 -0400

    proposition to fix #197

--- raytracing/imagingpath.py
+++ raytracing/imagingpath.py
@@ -161,7 +161,7 @@ class ImagingPath(MatrixGroup):
         A = transferMatrixToApertureStop.A
         B = transferMatrixToApertureStop.B
 
-        if B == 0:
+        if transferMatrixToApertureStop.isImaging:
             return None
 
         if y is None:
