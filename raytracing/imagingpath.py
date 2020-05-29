4fa8b24bda05129b69c3848e534113b0bde5d3bf
Gabriel Genest <gabriel.genest.2@ulaval.ca>
2020-05-29 16:20:45 -0400

    code was mixed up when updated from master

--- raytracing/imagingpath.py
+++ raytracing/imagingpath.py
@@ -264,11 +264,10 @@ class ImagingPath(MatrixGroup):
         A = transferMatrixToApertureStop.A
         B = transferMatrixToApertureStop.B
 
-        thetaUp = (stopDiameter / 2.0 - A * y) / B
         if transferMatrixToApertureStop.isImaging:
             return None
 
-        thetaUp  = (stopDiameter / 2.0 - A * y) / B
+        thetaUp = (stopDiameter / 2.0 - A * y) / B
         thetaDown = (-stopDiameter / 2.0 - A * y) / B
 
         if thetaDown > thetaUp:
