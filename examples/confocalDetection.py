e29fb3bb396d2e96b5ca2c419ab6ba2c7e93be2e
Valérie <valerie.pineau-noel.1@ulaval.ca>
2020-06-01 11:39:04 -0400

    confocalDetection.py put illumination into function
    Allows the using of imageSize(), but not sure if it's the best way of doing it.

--- examples/confocalDetection.py
+++ examples/confocalDetection.py
@@ -8,9 +8,6 @@ To obtain and plot the intensity of a point source at the pinhole of a confocal 
  as a function of position of focal spot by sending a large number of rays in the system (changing the position of the
  focal spot provides an optical sectioning process).
 """
-#import os
-#import sys
-#sys.path.insert(0, os.path.abspath('../'))
 
 # Dictionary of pinhole factors with an empty list which will subsequently contain the transmission efficiency
 # for each focal spot position
@@ -23,40 +20,72 @@ nRays = 10000
 # Production of rays from a focal spot with a radius of 250 nm
 inputRays = RandomUniformRays(yMax=0.000250, yMin=-0.000250, maxCount=nRays)
 
+objFocalLength = 5
+
+def optimalPinholeSize():
+
+    illumination = ImagingPath()
+    illumination.append(Space(d=objFocalLength))
+    illumination.append(Lens(f=objFocalLength))
+    illumination.append(Space(d=105))
+    illumination.append(Lens(f=100))
+    illumination.append(Space(d=200))
+    illumination.append(Lens(f=100))
+    illumination.append(Space(d=175))
+    illumination.append(Lens(f=75))
+    illumination.append(Space(d=115))
+    illumination.append(Lens(f=40))
+    illumination.append(Space(d=90))
+    illumination.append(Lens(f=50))
+    illumination.append(Space(d=50))
+    pinholeIdeal = illumination.imageSize()
+    illumination.append(Aperture(diameter=pinholeIdeal))
+
+    return pinholeIdeal
+
+def illuminationPath(pinholeFactor=None, focalspot=None):
+
+    illumination = ImagingPath()
+    illumination.append(Space(d=focalspot))
+    illumination.append(Lens(f=objFocalLength))
+    illumination.append(Space(d=105))
+    illumination.append(Lens(f=100))
+    illumination.append(Space(d=200))
+    illumination.append(Lens(f=100))
+    illumination.append(Space(d=175))
+    illumination.append(Lens(f=75))
+    illumination.append(Space(d=115))
+    illumination.append(Lens(f=40))
+    illumination.append(Space(d=90))
+    illumination.append(Lens(f=50))
+    illumination.append(Space(d=50))
+
+    pinholeSize = optimalPinholeSize() * pinholeFactor
+
+    print(pinholeSize)
+
+    illumination.append(Aperture(diameter=pinholeSize))
+
+    # Counts how many rays make it through the pinhole
+    outputRays = illumination.traceManyThrough(inputRays, progress=False)
+    finalRays.append(outputRays.count/inputRays.count)  # Calculates the transmission efficiency
+
+
+    return illumination
+
+
 for pinhole in pinholeModifier:
 
     finalRays = pinholeModifier[pinhole]
-    # pinholeSize = 0.009374*pinhole  # Ideal size of the pinhole times a certain factor
     print(".")
 
     for z in positions:
 
-        newPositions = 5 + (z*0.000001)  # Working distance of the objective + position
+        newPosition = 5 + (z*0.000001)  # Working distance of the objective + position
         #print(newPositions)
 
-        illumination = ImagingPath()
-        illumination.append(Space(d=newPositions))
-        illumination.append(Lens(f=5))
-        illumination.append(Space(d=105))
-        illumination.append(Lens(f=100))
-        illumination.append(Space(d=200))
-        illumination.append(Lens(f=100))
-        illumination.append(Space(d=175))
-        illumination.append(Lens(f=75))
-        illumination.append(Space(d=115))
-        illumination.append(Lens(f=40))
-        illumination.append(Space(d=90))
-        illumination.append(Lens(f=50))
-        illumination.append(Space(d=50))
-        pinholeSize = 0.009375 * pinhole
-        #pinholeSize = illumination.imageSize()*pinhole  # Ideal size of the pinhole times a certain factor
-        #print(pinholeSize)
-        #print(illumination.imageSize())
-        illumination.append(Aperture(diameter=pinholeSize))
+        illuminationPath(pinholeFactor = pinhole, focalspot=newPosition)
 
-        # Counts how many rays make it through the pinhole
-        outputRays = illumination.traceManyThrough(inputRays, progress=False)
-        finalRays.append(outputRays.count/inputRays.count)  # Calculates the transmission efficiency
 
     pinholeModifier[pinhole] = finalRays  # Incorporates the final list of transmission efficiencies into the dictionary
 
