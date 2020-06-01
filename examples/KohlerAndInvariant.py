83cab61709fffb3eeb64045c12e056e0b38f898f
Valérie <valerie.pineau-noel.1@ulaval.ca>
2020-06-01 13:30:28 -0400

    KohlerAndInvariant.py adding code
    - Added Sample imaging path for the same system.
    - Modified the source imaging path.
    - Modified fanAngle

--- examples/KohlerAndInvariant.py
+++ examples/KohlerAndInvariant.py
@@ -1,6 +1,4 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../'))
+import envexamples
 
 from raytracing import *
 
@@ -8,33 +6,48 @@ from raytracing import *
 DESCRIPTION
 '''
 
-#illumination = ImagingPath()
+# Source with Kohler illumination 
+illumination1 = ImagingPath()
+illumination1.fanNumber = 1
+illumination1.fanAngle = 5
 
-#illumination.append(Space(d=10))
-#illumination.append(Lens(f=10, diameter=100, label="Collector"))
-#illumination.append(Space(d=10+30))
-#illumination.append(Lens(f=30, diameter=100, label="Condenser"))
-#illumination.append(Space(d=30+7))
-#illumination.append(Lens(f=7, diameter=100, label="Objective"))
-#illumination.append(Space(d=7+30))
-#illumination.append(Lens(f=30, diameter=100, label="Tube"))
-#illumination.append(Space(d=30+10))
-#illumination.append(Lens(f=10, diameter=100, label="Eyepiece"))
-#illumination.append(Space(d=10))
-#illumination.append(Aperture(diameter=20, label="Eye entrance"))
-#illumination.append(Space(d=10))
+illumination1.append(Space(d=10))
+illumination1.append(Lens(f=10, diameter=100, label="Collector"))
+illumination1.append(Space(d=10+30))
+illumination1.append(Lens(f=30, diameter=100, label="Condenser"))
+illumination1.append(Space(d=30+30))
+illumination1.append(Lens(f=30, diameter=100, label="Objective"))
+illumination1.append(Space(d=30+30))
+illumination1.append(Lens(f=30, diameter=100, label="Tube"))
+illumination1.append(Space(d=30+30))
+illumination1.append(Lens(f=30, diameter=100, label="Eyepiece"))
+illumination1.append(Space(d=30+2))
+illumination1.append(Lens(f=2, diameter=10, label="Eye Entrance"))
+illumination1.append(Space(d=2))
 
-#illumination.display(limitObjectToFieldOfView=True, onlyChiefAndMarginalRays=True)
+print("The Lagrange Invariant of this system is {}".format(illumination1.lagrangeInvariant()))
+illumination1.display()
 
-path = ImagingPath()
-path.label="(a) Vignetting with FS poorly placed because of second lens diameter"
-path.append(Space(d=50))
-path.append(Lens(f=50, diameter=15, label="First lens"))
-path.append(Space(d=100))
-path.append(Lens(f=50, diameter=15, label="Second lens"))
-path.append(Space(d=50))
-path.append(Aperture(diameter=10, label='Camera'))
-path.append(Space(d=50))
 
-print("The Lagrange Invariant of this system is {}".format(path.lagrangeInvariant()))
-path.display(limitObjectToFieldOfView=True, onlyChiefAndMarginalRays=True)
+# Sample imaging path 
+illumination2 = ImagingPath()
+illumination2.objectPosition = 140
+illumination2.objectHeight = 5
+illumination2.fanNumber = 1
+illumination2.fanAngle = 0
+
+illumination2.append(Space(d=10))
+illumination2.append(Lens(f=10, diameter=100, label="Collector"))
+illumination2.append(Space(d=10+30))
+illumination2.append(Lens(f=30, diameter=100, label="Condenser"))
+illumination2.append(Space(d=30+30))
+illumination2.append(Lens(f=30, diameter=100, label="Objective"))
+illumination2.append(Space(d=30+30))
+illumination2.append(Lens(f=30, diameter=100, label="Tube"))
+illumination2.append(Space(d=30+30))
+illumination2.append(Lens(f=30, diameter=100, label="Eyepiece"))
+illumination2.append(Space(d=30+2))
+illumination2.append(Lens(f=2, diameter=10, label="Eye Entrance"))
+illumination2.append(Space(d=2))
+
+illumination2.display()
