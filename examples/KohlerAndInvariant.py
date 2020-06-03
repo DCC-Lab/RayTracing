8c21f8b9758f3c7e3c065b85d53f914d60197b3e
Valérie <valerie.pineau-noel.1@ulaval.ca>
2020-06-03 15:48:01 -0400

    KohlerAndInvariant.py added display() - Talk with LudoK

--- examples/KohlerAndInvariant.py
+++ examples/KohlerAndInvariant.py
@@ -27,8 +27,8 @@ def displayMultiple(paths: list,
 # Source with Kohler illumination 
 illumination1 = ImagingPath()
 illumination1.fanNumber = 3
-illumination1.fanAngle = 5
-#illumination1.design['rayColors'] = ['b', 'b', 'b']
+illumination1.fanAngle = 0.1
+illumination1.design['rayColors'] = ['b', 'b', 'b']
 
 illumination1.append(Space(d=10))
 illumination1.append(Lens(f=10, diameter=100, label="Collector"))
@@ -43,6 +43,7 @@ illumination1.append(Lens(f=30, diameter=100, label="Eyepiece"))
 illumination1.append(Space(d=30+2))
 illumination1.append(Lens(f=2, diameter=10, label="Eye Entrance"))
 illumination1.append(Space(d=2))
+illumination1.display()
 
 
 
@@ -50,9 +51,9 @@ illumination1.append(Space(d=2))
 # Source without Kohler illumination 
 illumination2 = ImagingPath()
 illumination2.fanNumber = 3
-illumination2.fanAngle = 5
+illumination2.fanAngle = 0.1
 illumination2.objectPosition = 20
-#illumination2.design['rayColors'] = ['g', 'g', 'g']
+illumination2.design['rayColors'] = ['g', 'g', 'g']
 
 illumination2.append(Space(d=30))
 illumination2.append(Lens(f=30, diameter=100, label="Condenser"))
@@ -65,17 +66,18 @@ illumination2.append(Lens(f=30, diameter=100, label="Eyepiece"))
 illumination2.append(Space(d=30+2))
 illumination2.append(Lens(f=2, diameter=10, label="Eye Entrance"))
 illumination2.append(Space(d=2))
+illumination2.display()
 
 
 
 
 # Sample imaging path 
 illumination3 = ImagingPath()
-illumination3.objectPosition = 140
+illumination3.objectPosition = 80
 illumination3.objectHeight = 5
 illumination3.fanNumber = 1
 illumination3.fanAngle = 0
-#illumination3.design['rayColors'] = ['r', 'r', 'r']
+illumination3.design['rayColors'] = ['r', 'r', 'r']
 
 
 illumination3.append(Space(d=10))
@@ -91,6 +93,7 @@ illumination3.append(Lens(f=30, diameter=100, label="Eyepiece"))
 illumination3.append(Space(d=30+2))
 illumination3.append(Lens(f=2, diameter=10, label="Eye Entrance"))
 illumination3.append(Space(d=2))
+illumination3.display()
 
-displayMultiple([illumination3, illumination1])
-displayMultiple([illumination3, illumination2])
+#displayMultiple([illumination3, illumination1])
+#displayMultiple([illumination3, illumination2])
