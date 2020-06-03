50c0e1dd4d9467ff5384d62a58ead2cff3469388
ElaheParham <59700098+ElaheParham@users.noreply.github.com>
2020-06-03 11:37:04 -0400

    Updated documentation for LaserPath.py

--- raytracing/laserpath.py
+++ raytracing/laserpath.py
@@ -3,7 +3,7 @@ from .imagingpath import *
 
 
 class LaserPath(MatrixGroup):
-    """LaserPath: the main class of the module for coherent
+    """The main class of the module for coherent
     laser beams: it is the combination of Matrix() or MatrixGroup()
     to be used as a laser path with a laser beam (GaussianBeam)
     at the entrance.
@@ -12,6 +12,34 @@ class LaserPath(MatrixGroup):
     and display(). You may change the inputBeam to any GaussianBeam(),
     or provide one to display(beam=GaussianBeam())
 
+    Parameters
+    ----------
+    elements : list of elements
+        A list of ABCD matrices in the imaging path
+    label : string
+        the label for the imaging path (Optional)
+
+    Attributes
+    ----------
+    inputBeam : object of GaussianBeam class
+        the input beam of the imaging path is defined using this parameter.
+    isResonator : bool
+        #fixme : If True, what happens? (default=False)
+    showElementLabels : bool
+        If True, the labels of the elements will be shown on display. (default=True)
+    showPointsOfInterest : bool
+        If True, the points of interest will be shown on display. (default=True)
+    showPointsOfInterestLabels : bool
+        If True, the labels of the points of interest will be shown on display. (default=True)
+    showPlanesAcrossPointsOfInterest : bool
+        If True, the planes across the points of interest will be shown on display. (default=True)
+
+    See Also
+    --------
+    raytracing.GaussianBeam
+
+    Notes
+    -----
     Gaussian laser beams are not "blocked" by aperture. The formalism
     does not explicitly allow that.  However, if it appears that a 
     GaussianBeam() would be clipped by  finite aperture, a property 
