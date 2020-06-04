69e0211932beec188f23c0c508b71d1aff7db70f
Valérie <valerie.pineau-noel.1@ulaval.ca>
2020-06-04 10:10:27 -0400

    widefield.py added output rays count
    - A function rayCount was added to count how many rays get through the path
    - Since the same path is used more than once, I made a function for the path and called the function many times with different values of lens diameter. Makes the code much simpler I think.

--- examples/widefield.py
+++ examples/widefield.py
@@ -21,40 +21,48 @@ paths containing different lens diameter.
 import envexamples
 from raytracing import *
 
-path = ImagingPath()
-path.label="(a) Vignetting with FS poorly placed because of second lens diameter"
-path.append(Space(d=50))
-path.append(Lens(f=50, diameter=15, label="First lens"))
-path.append(Space(d=100))
-path.append(Lens(f=50, diameter=15, label="Second lens"))
-path.append(Space(d=50))
-path.append(Aperture(diameter=10, label='Camera'))
-path.append(Space(d=50))
-print("The Lagrange Invariant of this system is {}".format(path.lagrangeInvariant()))
-path.display(limitObjectToFieldOfView=True, onlyChiefAndMarginalRays=True)
+def rayCount(nRays=10000, objectHalfHeight=5, path=ImagingPath()):
+
+    inputRays = RandomUniformRays(yMax=objectHalfHeight, yMin=objectHalfHeight, maxCount=nRays)
+    outputRays = Rays()
+
+    for ray in inputRays:
+        lastRay = path.traceThrough(ray)
+        if lastRay.isNotBlocked:
+            outputRays.append(lastRay)
+    inputRays.displayProgress()
+
+    print("Number of rays sent: {0}".format(inputRays.count))
+    print("Number of rays received: {0}".format(outputRays.count))
+    print("Transmission efficiency: {0}".format(outputRays.count/inputRays.count))
+    
+
+    return outputRays.display("Output profile")
+
+def imagingPath(a=10, b=10, title=""):
+    
+    path = ImagingPath()
+    path.label=title
+    path.append(Space(d=50))
+    path.append(Lens(f=50, diameter=a, label="First lens"))
+    path.append(Space(d=100))
+    path.append(Lens(f=50, diameter=b, label="Second lens"))
+    path.append(Space(d=50))
+    path.append(Aperture(diameter=10, label='Camera'))
+    path.append(Space(d=50))
+    
+    return path
 
 
-path = ImagingPath()
-path.label="(b) Suboptimal AS at second lens, but without vignetting"
-path.append(Space(d=50))
-path.append(Lens(f=50, diameter=40, label="First lens"))
-path.append(Space(d=100))
-path.append(Lens(f=50, diameter=15, label="Second lens"))
-path.append(Space(d=50))
-path.append(Aperture(diameter=10, label='Camera'))
-path.append(Space(d=50))
-print("The Lagrange Invariant of this system is {}".format(path.lagrangeInvariant()))
-path.display(limitObjectToFieldOfView=True, onlyChiefAndMarginalRays=True)
 
+path1 = imagingPath(a=15, b=15, title="(a) Vignetting with FS poorly placed because of second lens diameter")
+path1.display(limitObjectToFieldOfView=True, onlyChiefAndMarginalRays=True)
+rayCount(path=path1)
 
-path = ImagingPath()
-path.label="(c) Optimal AS at first lens and FS at Camera"
-path.append(Space(d=50))
-path.append(Lens(f=50, diameter=25, label="First lens"))
-path.append(Space(d=100))
-path.append(Lens(f=50, diameter=25, label="Second lens"))
-path.append(Space(d=50))
-path.append(Aperture(diameter=10, label='Camera'))
-path.append(Space(d=50))
-print("The Lagrange Invariant of this system is {}".format(path.lagrangeInvariant()))
-path.display(limitObjectToFieldOfView=True, onlyChiefAndMarginalRays=True)
+path2 = imagingPath(a=40, b=15, title="(b) Suboptimal AS at second lens, but without vignetting")
+path2.display(limitObjectToFieldOfView=True, onlyChiefAndMarginalRays=True)
+rayCount(path=path2)
+
+path3 = imagingPath(a=25, b=25, title="(c) Optimal AS at first lens and FS at Camera")
+path3.display(limitObjectToFieldOfView=True, onlyChiefAndMarginalRays=True)
+rayCount(path=path3)
