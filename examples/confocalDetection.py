d0e962753971d912fc33d6a07ba87b316faa971a
JLBegin <ludovick.begin@gmail.com>
2020-06-08 17:00:02 -0400

    pepfix

--- examples/confocalDetection.py
+++ examples/confocalDetection.py
@@ -9,20 +9,20 @@ To obtain and plot the intensity of a point source at the pinhole of a confocal 
  focal spot provides an optical sectioning process).
 """
 
-
 # Focal spot radius (Airy disk radius)
 focalRadius = 0.000250
 # Dictionary of pinhole factors with an empty list which will subsequently contain the transmission efficiency
 # for each focal spot position
-pinholeModifier = {1/3: [], 1: [], 3: []}
+pinholeModifier = {1 / 3: [], 1: [], 3: []}
 # list of all relative positions from the ideal focal spot position in nm
 positions = [1000, 800, 500, 300, 150, 100, 50, 25, 0, -25, -50, -100, -150, -300, -500, -800, -1000]
 # Number of total rays produced by the focal spot
-nRays = 10000 
+nRays = 10000
 # Production of rays from a focal spot with a radius determined by focalRadius
 inputRays = RandomUniformRays(yMax=focalRadius, yMin=-focalRadius, maxCount=nRays)
 # Focal length of the objective
 objFocalLength = 5
+
 
 def optimalPinholeSize():
     """
@@ -48,16 +48,17 @@ def optimalPinholeSize():
     illumination.append(Lens(f=40))
     illumination.append(Space(d=90))
     illumination.append(Lens(f=50))
-    illumination.append(Space(d=50)) # Path finishes at the pinhole position
+    illumination.append(Space(d=50))  # Path finishes at the pinhole position
 
     # Dictionnary of the position and magnification of all conjugate planes of the focal spot. 
-    planes = illumination.intermediateConjugates() 
+    planes = illumination.intermediateConjugates()
     # The last conjugate plane is the pinhole. The magnification of this position is saved in mag. 
     mag = planes[-1][1]
     # Calculates the pinhole size that fits perfectly the focal spot diameter.
-    pinholeIdeal = abs(mag*(focalRadius*2))
+    pinholeIdeal = abs(mag * (focalRadius * 2))
 
     return pinholeIdeal
+
 
 def illuminationPath(pinholeFactor=None, focalSpotPosition=None):
     """
@@ -76,7 +77,6 @@ def illuminationPath(pinholeFactor=None, focalSpotPosition=None):
         illumination : object of ImagingPath class.
             Returns the illumination path
     """
-
 
     illumination = ImagingPath()
     illumination.append(Space(d=focalSpotPosition))
@@ -98,10 +98,9 @@ def illuminationPath(pinholeFactor=None, focalSpotPosition=None):
 
     # Counts how many rays make it through the pinhole
     outputRays = illumination.traceManyThrough(inputRays, progress=False)
-    finalRays.append(outputRays.count/inputRays.count)  # Calculates the transmission efficiency
+    finalRays.append(outputRays.count / inputRays.count)  # Calculates the transmission efficiency
 
     return illumination
-
 
 
 for pinhole in pinholeModifier:
@@ -110,17 +109,15 @@ for pinhole in pinholeModifier:
     print(".")
 
     for z in positions:
-
         # Working distance of the objective + position
-        newPosition = 5 + (z*0.000001)
+        newPosition = 5 + (z * 0.000001)
 
-        illuminationPath(pinholeFactor = pinhole, focalSpotPosition=newPosition)
-
+        illuminationPath(pinholeFactor=pinhole, focalSpotPosition=newPosition)
 
     pinholeModifier[pinhole] = finalRays  # Incorporates the final list of transmission efficiencies into the dictionary
 
 # Prints a plot of the three configurations. 
-plt.plot(positions, pinholeModifier[1/3], label='Pinhole Size S/3', linestyle='dashed')
+plt.plot(positions, pinholeModifier[1 / 3], label='Pinhole Size S/3', linestyle='dashed')
 plt.plot(positions, pinholeModifier[1], label='Pinhole Size S')
 plt.plot(positions, pinholeModifier[3], label='Pinhole Size 3S', linestyle='dotted')
 plt.ylabel('Transmission efficiency')
