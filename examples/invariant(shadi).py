2da311ab8b5450cb7f69c0c230b818239d43ed0b
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-06-09 18:30:11 -0400

    Create invariant(shadi).py

---
+++ examples/invariant(shadi).py
@@ -0,0 +1,14 @@
+import envexamples
+from raytracing import *
+
+path = ImagingPath()
+path.name = "4f system, 1 cm object, small lenses"
+path.append(Space(d=5))
+path.append(Lens(f=5, diameter=2.5))
+path.append(Space(d=15))
+path.append(Lens(f=10,diameter=2.5))
+path.append(Space(d=10))
+path.lagrangeInvariant()
+print(path.lagrangeInvariant())
+path.display()
+
