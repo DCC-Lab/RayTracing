566b4c68f3b7bec397e46a7959b8d9bc9592750b
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-06-10 16:15:53 -0400

    delete 2files

--- examples/RandomLambertianrays.py
+++
@@ -1,8 +0,0 @@
-import envexamples
-from raytracing import*
-nRays = 1000000 # Increase for better resolution
-minHeight=0
-maxHeight=50
-# define a list of rays with Lambertian distribution
-inputRays = RandomLambertianRays(yMin=minHeight, yMax=maxHeight, maxCount=nRays)
-inputRays.display()
