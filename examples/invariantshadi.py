f444b96339b0be2af1b6866271c0cf4d3bbdc76a
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-06-10 11:37:58 -0400

    Update invariantshadi.py

--- examples/invariantshadi.py
+++ examples/invariantshadi.py
@@ -10,7 +10,7 @@ path.append(Lens(f=10,diameter=50))
 path.append(Space(d=10))
 path.lagrangeInvariant()
 print(path.lagrangeInvariant())
-path.display(onlyChiefAndMarginalRays=True, limitObjectToFieldOfView=False)
+path.display(onlyPrincipalAndAxialRays=True, limitObjectToFieldOfView=False)
 
 path1 = ImagingPath()
 path.objectHeight = 20
@@ -21,7 +21,7 @@ path1.append(Lens(f=10,diameter=50))
 path1.append(Space(d=10))
 path1.lagrangeInvariant()
 print(path1.lagrangeInvariant())
-path1.display(onlyChiefAndMarginalRays=True, limitObjectToFieldOfView=False)
+path1.display(onlyPrincipalAndAxialRays=True, limitObjectToFieldOfView=False)
 
 path2 = ImagingPath()
 path.objectHeight = 10
@@ -32,7 +32,7 @@ path2.append(Lens(f=10,diameter=10))
 path2.append(Space(d=10))
 path2.lagrangeInvariant()
 print(path2.lagrangeInvariant())
-path2.display(onlyChiefAndMarginalRays=True, limitObjectToFieldOfView=False)
+path2.display(onlyPrincipalAndAxialRays=True, limitObjectToFieldOfView=False)
 
 
 path3 = ImagingPath()
@@ -44,5 +44,5 @@ path3.append(Lens(f=10,diameter=0.5))
 path3.append(Space(d=10))
 path3.lagrangeInvariant()
 print(path3.lagrangeInvariant())
-path3.display(onlyChiefAndMarginalRays=True, limitObjectToFieldOfView=False)
+path3.display(onlyPrincipalAndAxialRays=True, limitObjectToFieldOfView=False)
 
