8f7ba74787cc7a0f77b2377ae0682b5119f748fe
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-06-10 11:00:35 -0400

    Update invariantshadi.py

--- examples/invariantshadi.py
+++ examples/invariantshadi.py
@@ -1,18 +1,6 @@
 import envexamples
 from raytracing import *
 
-
-path = ImagingPath()
-path.objectHeight = 2.5
-path.append(Space(d=5))
-path.append(Lens(f=5, diameter=2.5))
-path.append(Space(d=15))
-path.append(Lens(f=10,diameter=2.5))
-path.append(Space(d=10))
-path.lagrangeInvariant()
-print(path.lagrangeInvariant())
-path.display()
-
 path1 = ImagingPath()
 path1.append(Space(d=5))
 path1.append(Lens(f=5, diameter=2.5))
@@ -21,7 +9,7 @@ path1.append(Lens(f=10,diameter=2.5))
 path1.append(Space(d=10))
 path1.lagrangeInvariant()
 print(path1.lagrangeInvariant())
-path1.display()
+path1.display(path.display(onlyChiefAndMarginalRays=True, limitObjectToFieldOfView=True))
 
 
 path2 = ImagingPath()
@@ -32,7 +20,7 @@ path2.append(Lens(f=10,diameter=1))
 path2.append(Space(d=10))
 path2.lagrangeInvariant()
 print(path2.lagrangeInvariant())
-path2.display()
+path2.display(path.display(onlyChiefAndMarginalRays=True, limitObjectToFieldOfView=True))
 
 
 path3 = ImagingPath()
@@ -42,7 +30,7 @@ path3.append(Space(d=15))
 path3.append(Lens(f=10,diameter=0.5))
 path3.append(Space(d=10))
 path3.lagrangeInvariant()
-print(path3.lagrangeInvariant())
+print(path3.lagrangeInvariant(path.display(onlyChiefAndMarginalRays=True, limitObjectToFieldOfView=True)))
 path3.display()
 
 
