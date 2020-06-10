5da65cf52a65949a8a611ef1531760478a155ffc
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-06-10 10:06:22 -0400

    Update invariantshadi.py

--- examples/invariantshadi.py
+++ examples/invariantshadi.py
@@ -1,14 +1,35 @@
 import envexamples
 from raytracing import *
 
-path = ImagingPath()
-path.objectHeight = 0.5
-path.append(Space(d=5))
-path.append(Lens(f=5, diameter=2.5))
-path.append(Space(d=15))
+path1 = ImagingPath()
+path1.append(Space(d=5))
+path1.append(Lens(f=5, diameter=2.5))
+path1.append(Space(d=15))
 path.append(Lens(f=10,diameter=2.5))
-path.append(Space(d=10))
-path.lagrangeInvariant()
-print(path.lagrangeInvariant())
-path.display()
+path1.append(Space(d=10))
+path1.lagrangeInvariant()
+print(path1.lagrangeInvariant())
+path1.display()
+
+
+path2 = ImagingPath()
+path2.append(Space(d=5))
+path2.append(Lens(f=5, diameter=1))
+path2.append(Space(d=15))
+path2.append(Lens(f=10,diameter=1))
+path2.append(Space(d=10))
+path2.lagrangeInvariant()
+print(path2.lagrangeInvariant())
+path2.display()
+
+
+path3 = ImagingPath()
+path3.append(Space(d=5))
+path3.append(Lens(f=5, diameter=2))
+path3.append(Space(d=15))
+path3.append(Lens(f=10,diameter=2))
+path3.append(Space(d=10))
+path3.lagrangeInvariant()
+print(path3.lagrangeInvariant())
+path3.display()
 
