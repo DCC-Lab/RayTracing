d8ddcef2f54326e6e6d936b26b53d09a5be07d1a
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-06-11 14:08:54 -0400

    Update TPDD.py

--- examples/TPDD.py
+++ examples/TPDD.py
@@ -5,12 +5,13 @@ nRays = 1000000
 minHeight=-0.5
 maxHeight=0.5
 inputRays = RandomLambertianRays(yMin=minHeight, yMax=maxHeight)
-path = ImagingPath()
-path.append(Space(d=20))
-path.append(Lens(f=20,diameter=8))
-path.append(Space(d=45))
-path.append(Lens(f=25, diameter=8))
-path.append(Space(d=25))
-path.append(Aperture(diameter=2, label='Camera'))
-path.display(onlyPrincipalAndAxialRays=False, limitObjectToFieldOfView=False)
+  
+  path = ImagingPath()
+  path.append(Space(d=20))
+  path.append(Lens(f=20,diameter=8))
+  path.append(Space(d=45))
+  path.append(Lens(f=25, diameter=8))
+  path.append(Space(d=25))
+  path.append(Aperture(diameter=2, label='Camera'))
+  path.display(onlyPrincipalAndAxialRays=False, limitObjectToFieldOfView=False)
  
