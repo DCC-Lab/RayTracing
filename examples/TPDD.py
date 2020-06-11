68c8bdcd40bedf44bf946a9e359da21acd15e802
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-06-11 11:52:12 -0400

    Update TPDD.py

--- examples/TPDD.py
+++ examples/TPDD.py
@@ -4,11 +4,9 @@ from raytracing import *
 nRays = 1000000
 minHeight=-0.5
 maxHeight=0.5
-
 inputRays = RandomLambertianRays(yMin=minHeight, yMax=maxHeight)
 path = ImagingPath()
 path.name = "..."
-path.objectHeight = 1.0
 path.append(System4f(f1=50, diameter1=10, f2=50, diameter2=10))
 path.append(Aperture(diameter=5, label='Camera'))
-path.display(onlyChiefAndMarginalRays=True, limitObjectToFieldOfView=False)
+path.display(onlyPrincipalAndAxialRays=True, limitObjectToFieldOfView=False)
