f8680b61e0d35f63d1117c6a6cc99507abd936b3
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-06-11 11:59:41 -0400

    Update TPDD.py

--- examples/TPDD.py
+++ examples/TPDD.py
@@ -9,4 +9,4 @@ inputRays = RandomLambertianRays(yMin=minHeight, yMax=maxHeight)
 path.name = "..."
 path.append(System4f(f1=50, diameter1=10, f2=50, diameter2=10))
 path.append(Aperture(diameter=5, label='Camera'))
-path.display(onlyPrincipalAndAxialRays=True, limitObjectToFieldOfView=False)
+path.display(onlyPrincipalAndAxialRays=False, limitObjectToFieldOfView=False)
