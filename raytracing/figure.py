74064810c556ea1fc187f68bd8357c379148c143
Daniel Côté <dccote@novajo.ca>
2020-06-11 11:34:05 -0400

    Display() is broken with LaserPath
    Modifications have been made to preparer Figure for LaserPath.  Not complete, help needed.

--- raytracing/figure.py
+++ raytracing/figure.py
@@ -143,6 +143,122 @@ class Figure:
         else:
             self._showPlot()
 
+    def displayGaussianBeam(self, inputBeams=None, comments=None):  # pragma: no cover
+        """ Display the optical system and trace the laser beam. 
+        If comments are included they will be displayed on a
+        graph in the bottom half of the plot.
+
+        Parameters
+        ----------
+        inputBeams : list of object of GaussianBeam class
+            A list of Gaussian beams
+        comments : string
+            If comments are included they will be displayed on a graph in the bottom half of the plot. (default=None)
+
+        """
+
+        self.initializeDisplay()
+
+        self.createBeamTracePlot(axes=axes, beams=beams)
+        self.drawDisplayObjects()
+
+        self.axes.callbacks.connect('ylim_changed', self.onZoomCallback)
+        self.axes.set_ylim([-self.displayRange() / 2 * 1.5, self.displayRange() / 2 * 1.5])
+
+        if filepath is not None:
+            self.figure.savefig(filepath, dpi=600)
+        else:
+            self._showPlot()
+
+
+        self._showPlot()
+
+    def createBeamTracePlot(self, axes, beams):  # pragma: no cover
+        """ Create a matplotlib plot to draw the laser beam and the elements.
+        """
+
+        displayRange = 2 * self.largestDiameter
+        if displayRange == float('+Inf'):
+            displayRange = self.inputBeam.w * 6
+
+        axes.set(xlabel='Distance', ylabel='Height', title=self.label)
+        axes.set_ylim([-displayRange / 2 * 1.2, displayRange / 2 * 1.2])
+
+        self.drawAt(z=0, axes=axes)
+
+        for beam in beams:
+            self.drawBeamTrace(axes, beam)
+            self.drawWaists(axes, beam)
+
+        return axes
+
+    def rearrangeBeamTraceForPlotting(self, rayList):
+        x = []
+        y = []
+        for ray in rayList:
+            x.append(ray.z)
+            y.append(ray.w)
+        return (x, y)
+
+    def drawBeamTrace(self, axes, beam):  # pragma: no cover
+        """ Draw beam trace corresponding to input beam 
+        Because the laser beam diffracts through space, we cannot
+        simply propagate the beam over large distances and trace it
+        (as opposed to rays, where we can). We must split Space() 
+        elements into sub elements to watch the beam size expand.
+        
+        We arbitrarily split Space() elements into N sub elements
+        before plotting.
+        """
+
+        N = 100
+        highResolution = ImagingPath()
+        for element in self.elements:
+            if isinstance(element, Space):
+                for i in range(N):
+                    highResolution.append(Space(d=element.L / N,
+                                                n=element.frontIndex))
+            else:
+                highResolution.append(element)
+
+        beamTrace = highResolution.trace(beam)
+        (x, y) = self.rearrangeBeamTraceForPlotting(beamTrace)
+        axes.plot(x, y, 'r', linewidth=1)
+        axes.plot(x, [-v for v in y], 'r', linewidth=1)
+
+    def drawWaists(self, axes, beam):  # pragma: no cover
+        """ Draws the expected waist (i.e. the focal spot or the spot where the
+        size is minimum) for all positions of the beam. This will show "waists" that
+        are virtual if there is an additional lens between the beam and the expceted
+        waist.
+
+        It is easy to obtain the waist position from the complex radius of curvature
+        because it is the position where the complex radius is imaginary. The position
+        returned is relative to the position of the beam, which is why we add the actual
+        position of the beam to the relative position. """
+
+        (xScaling, yScaling) = self.axesToDataScale(axes)
+        arrowWidth = xScaling * 0.01
+        arrowHeight = yScaling * 0.03
+        arrowSize = arrowHeight * 3
+
+        beamTrace = self.trace(beam)
+        for beam in beamTrace:
+            relativePosition = beam.waistPosition
+            position = beam.z + relativePosition
+            size = beam.waist
+
+            axes.arrow(position, size + arrowSize, 0, -arrowSize,
+                       width=0.1, fc='g', ec='g',
+                       head_length=arrowHeight, head_width=arrowWidth,
+                       length_includes_head=True)
+            axes.arrow(position, -size - arrowSize, 0, arrowSize,
+                       width=0.1, fc='g', ec='g',
+                       head_length=arrowHeight, head_width=arrowWidth,
+                       length_includes_head=True)
+
+
+
     def displayRange(self):
         """ We return the largest object in the ImagingPath for display purposes.
         The object is considered only "half" because it starts on axis and goes up.
