bc4f5f1141d927d940387f81e5d249eb1f7f8109
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-06-11 11:26:24 -0400

    Create Twophotondescanneddetector.py

---
+++ examples/Twophotondescanneddetector .py
@@ -0,0 +1,14 @@
+import envexamples
+from raytracing import *
+
+nRays = 1000000
+minHeight=-0.5
+maxHeight=0.5
+
+inputRays = RandomLambertianRays(yMin=minHeight, yMax=maxHeight)
+path = ImagingPath()
+path.name = "..."
+path.objectHeight = 1.0
+path.append(System4f(f1=50, diameter1=10, f2=50, diameter2=10))
+path.append(Aperture(diameter=10, label='Camera'))
+path.display(onlyChiefAndMarginalRays=True, limitObjectToFieldOfView=True)
