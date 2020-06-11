d7c193628db8a8754fa44b4ce679762e37e05860
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-06-11 11:57:16 -0400

    Update TPDD.py

--- examples/TPDD.py
+++ examples/TPDD.py
@@ -1,11 +1,11 @@
 import envexamples
 from raytracing import *
 
+path = ImagingPath()
 nRays = 1000000
 minHeight=-0.5
 maxHeight=0.5
 inputRays = RandomLambertianRays(yMin=minHeight, yMax=maxHeight)
-path = ImagingPath()
 path.name = "..."
 path.append(System4f(f1=50, diameter1=10, f2=50, diameter2=10))
 path.append(Aperture(diameter=5, label='Camera'))
