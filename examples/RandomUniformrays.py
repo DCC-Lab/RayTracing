cc55ca3f517cadbc6d8ced33c8443661039abbee
Daniel Côté <dccote@novajo.ca>
2020-06-10 21:27:47 -0400

    Layout

--- examples/RandomUniformrays.py
+++ examples/RandomUniformrays.py
@@ -1,10 +1,16 @@
 import envexamples
 from raytracing import *
+
 nRays = 1000000 # Increase for better resolution
-minHeight=0
-maxHeight=50
-minTheta=-1.5
-maxTheta=1.5
+minHeight = -5
+maxHeight = 5
+minTheta = -0.5 # rad
+maxTheta = +0.5
+
 # define a list of rays with uniform distribution
-inputRays = RandomUniformRays(yMin=minHeight, yMax=maxHeight, maxCount=nRays, thetaMax=maxTheta, thetaMin=minTheta)
+inputRays = RandomUniformRays(yMin = minHeight, 
+	                          yMax = maxHeight,
+	                          maxCount = nRays,
+	                          thetaMax = maxTheta,
+	                          thetaMin = minTheta)
 inputRays.display()
