1fc15bb6ed3dc95c52484928c283a63f7f7d8597
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-06-16 18:53:19 -0400

    Update TPDD.py

--- examples/TPDD.py
+++ examples/TPDD.py
@@ -9,15 +9,11 @@ import matplotlib.pyplot as plt
 nRays = 1000
 minHeight=-0.5
 maxHeight=0.5
-inputRays = RandomLambertianRays(yMax=maxHeight, yMin=-minHeight, maxCount=nRays)
+inputRays = RandomLambertianRays(yMax=maxHeight, yMin=minHeight, maxCount=nRays)
 path = ImagingPath()
-path.append(Space(d=120))
-path.append(Lens(f=100,diameter=20))
-path.append(Space(d=175))
-path.append(Lens(f=75, diameter=20))
-path.append(Space(d=75))
-path.append(Aperture(diameter=4, label='Camera'))
-# Counts how many rays make it through the detector
-outputRays = path.traceManyThroughInParallel(inputRays, progress=False)
-path.display()
-outputRays.display()
+path.append(System4f(f1=50, diameter1=25, f2=50, diameter2=50))
+path.append(Aperture(diameter=20, label='Camera'))
+outputRays = path.traceManyThrough(inputRays, progress=False)
+efficiency = 100*outputRays.count/inputRays.count
+path.display(limitObjectToFieldOfView=False, onlyPrincipalAndAxialRays=True)
+outputRays.display("Output profile {0:.0f}% efficiency".format(efficiency), showTheta=False)
