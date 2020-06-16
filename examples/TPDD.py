f9bcf027872d35a9c9b98c6d171e951d6ed9f33b
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-06-16 17:05:41 -0400

    Update TPDD.py

--- examples/TPDD.py
+++ examples/TPDD.py
@@ -23,5 +23,21 @@ outputRays = path.traceManyThroughInParallel(inputRays, progress=False)
 path.display()
 outputRays.display()
 
-
+###
+nRays = 1000000
+minHeight=-0.5
+maxHeight=0.5
+inputRays = RandomLambertianRays(yMax=maxHeight, yMin=-minHeight, maxCount=nRays)
+f=20
+path = ImagingPath()
+path.append(Space(d=f))
+path.append(Lens(f=f,diameter=f))
+path.append(Space(d=45))
+path.append(Lens(f=25, diameter=f))
+path.append(Space(d=25))
+path.append(Aperture(diameter=0.5, label='Camera'))
+# Counts how many rays make it through the detector
+outputRays = path.traceManyThroughInParallel(inputRays, progress=False)
+path.display()
+outputRays.display()
  
