4df76e4d3d61696bf21f002bc6fc8a781af8e174
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-06-16 18:06:30 -0400

    Update TPDD.py

--- examples/TPDD.py
+++ examples/TPDD.py
@@ -6,38 +6,18 @@ import matplotlib.pyplot as plt
 ........
 """
 
-nRays = 1000000
+nRays = 1000
 minHeight=-0.5
 maxHeight=0.5
 inputRays = RandomLambertianRays(yMax=maxHeight, yMin=-minHeight, maxCount=nRays)
-f=20
 path = ImagingPath()
-path.append(Space(d=f))
-path.append(Lens(f=f,diameter=f))
-path.append(Space(d=45))
-path.append(Lens(f=25, diameter=f))
-path.append(Space(d=25))
-path.append(Aperture(diameter=10, label='Camera'))
+path.append(Space(d=120))
+path.append(Lens(f=100,diameter=20))
+path.append(Space(d=175))
+path.append(Lens(f=75, diameter=20))
+path.append(Space(d=75))
+path.append(Aperture(diameter=4, label='Camera'))
 # Counts how many rays make it through the detector
 outputRays = path.traceManyThroughInParallel(inputRays, progress=False)
 path.display()
 outputRays.display()
-
-###
-nRays = 1000000
-minHeight=-0.5
-maxHeight=0.5
-inputRays = RandomLambertianRays(yMax=maxHeight, yMin=-minHeight, maxCount=nRays)
-f=20
-path = ImagingPath()
-path.append(Space(d=f))
-path.append(Lens(f=f,diameter=f))
-path.append(Space(d=45))
-path.append(Lens(f=25, diameter=f))
-path.append(Space(d=25))
-path.append(Aperture(diameter=2, label='Camera'))
-# Counts how many rays make it through the detector
-outputRays = path.traceManyThroughInParallel(inputRays, progress=False)
-path.display()
-outputRays.display()
- 
