8052679092c8a79d93f560f3bafaab928b8a0225
JLBegin <ludovick.begin@gmail.com>
2020-06-19 11:10:14 -0400

    return display of achromatDoubletLens to graphic

--- raytracing/specialtylenses.py
+++ raytracing/specialtylenses.py
@@ -137,6 +137,10 @@ class AchromatDoubletLens(MatrixGroup):
         (f1, f2) = self.focusPositions(z)
         return [{'z': f1, 'label': '$F_f$'}, {'z': f2, 'label': '$F_b$'}]
 
+    def display(self):
+        from .figure import AchromatDoubletLensGraphic
+        return AchromatDoubletLensGraphic(self).display()
+
 
 class SingletLens(MatrixGroup):
     """
