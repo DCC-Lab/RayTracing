ff3f165a8a99d56cf55efd68f2e973ecf40e60e6
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-07-13 14:02:16 -0400

    Update Twophotondescanneddetector.py

--- examples/Twophotondescanneddetector.py
+++ examples/Twophotondescanneddetector.py
@@ -19,7 +19,8 @@ path1.append(System4f(f1=50, diameter1=25, f2=50, diameter2=50))
 path1.append(Aperture(diameter=10, label='Camera'))
 outputRays = path1.traceManyThrough(inputRays, progress=False)
 efficiency = 100*outputRays.count/inputRays.count
-path1.display("Output profile {0:.0f}% efficiency".format(efficiency),limitObjectToFieldOfView=False, onlyPrincipalAndAxialRays=True)
+path1.display(limitObjectToFieldOfView=False, onlyPrincipalAndAxialRays=True)
+path1.label("Output profile {0:.0f}% efficiency".format(efficiency))
 outputRays.display("Output profile {0:.0f}% efficiency".format(efficiency), showTheta=False)
 print(efficiency)
 
