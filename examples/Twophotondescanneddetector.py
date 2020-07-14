416f6c3a65b892585b104c2a0cf32778241b360f
Valérie <valerie.pineau-noel.1@ulaval.ca>
2020-07-14 09:11:44 -0400

    Twophotondescanneddetector.py changed some variables

--- examples/Twophotondescanneddetector.py
+++ examples/Twophotondescanneddetector.py
@@ -3,55 +3,46 @@ from raytracing import *
 import matplotlib.pyplot as plt
 
 """
-The emitted light in the some optical systems like Two photon descanned detector is scattered in all directions.
-And the size of the diffused light dictates certain detectionlenses and detector size.  
-Therefore,  it is of great importance to find a well-sized detector that fits in the optical system.  
+The emitted light in some optical systems, like two-photon descanned detector, is scattered in all directions.
+The size of the diffused light dictates which lenses and detector size to choose.
+Therefore,  it is of great importance to find a well-sized detector that fits the optical system.  
 """
 
 #How to optimize the detector size ?
 
-
-
-###
 nRays = 100000
 minHeight=-0.5
 maxHeight=0.5
 inputRays = RandomLambertianRays(yMax=maxHeight, yMin=minHeight, maxCount=nRays)
+
+###
 path1 = ImagingPath()
 path1.append(System4f(f1=75, diameter1=50, f2=75, diameter2=75))
 path1.append(Aperture(diameter=0.5, label='Camera'))
-outputRays = path1.traceManyThrough(inputRays, progress=False)
-efficiency = 100*outputRays.count/inputRays.count
+outputRays1 = path1.traceManyThrough(inputRays, progress=False)
+efficiency1 = 100*outputRays1.count/inputRays.count
 path1.display(limitObjectToFieldOfView=False, onlyPrincipalAndAxialRays=True)
-outputRays.display("Output profile {0:.0f}% efficiency".format(efficiency), showTheta=False)
-print(efficiency)
+outputRays1.display("Output profile {0:.0f}% efficiency".format(efficiency1), showTheta=False)
+print(efficiency1)
 
 
 ###
-nRays = 100000
-minHeight=-0.5
-maxHeight=0.5
-inputRays = RandomLambertianRays(yMax=maxHeight, yMin=minHeight, maxCount=nRays)
 path2 = ImagingPath()
 path2.append(System4f(f1=50, diameter1=25, f2=50, diameter2=50))
 path2.append(Aperture(diameter=0.5, label='Camera'))
-outputRays = path2.traceManyThrough(inputRays, progress=False)
-efficiency = 100*outputRays.count/inputRays.count
+outputRays2 = path2.traceManyThrough(inputRays, progress=False)
+efficiency2 = 100*outputRays2.count/inputRays.count
 path2.display(limitObjectToFieldOfView=False, onlyPrincipalAndAxialRays=True)
-outputRays.display("Output profile {0:.0f}% efficiency".format(efficiency), showTheta=False)
-print(efficiency)
+outputRays2.display("Output profile {0:.0f}% efficiency".format(efficiency2), showTheta=False)
+print(efficiency2)
 
 
 ###
-nRays = 100000
-minHeight=-0.5
-maxHeight=0.5
-inputRays = RandomLambertianRays(yMax=maxHeight, yMin=minHeight, maxCount=nRays)
 path3 = ImagingPath()
 path3.append(System4f(f1=50, diameter1=25, f2=50, diameter2=50))
 path3.append(Aperture(diameter=10, label='Camera'))
-outputRays = path3.traceManyThrough(inputRays, progress=False)
-efficiency = 100*outputRays.count/inputRays.count
+outputRays3 = path3.traceManyThrough(inputRays, progress=False)
+efficiency3 = 100*outputRays3.count/inputRays.count
 path3.display(limitObjectToFieldOfView=False, onlyPrincipalAndAxialRays=True)
-outputRays.display("Output profile {0:.0f}% efficiency".format(efficiency), showTheta=False)
-print(efficiency)
+outputRays3.display("Output profile {0:.0f}% efficiency".format(efficiency3), showTheta=False)
+print(efficiency3)
