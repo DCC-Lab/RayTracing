5a14dcb95b1d6cb5b7ff8f6a9ff67d8cb3b8db5c
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-07-14 13:54:44 -0400

    create x.py

---
+++ raytracing/x.py
@@ -0,0 +1,9 @@
+path = ImagingPath()
+path.label = "Demo #5: Simple microscope system"
+path.append(Space(d=4))
+path.append(Lens(f=4, diameter=0.8, label='Obj'))
+path.append(Space(d=4 + 18))
+path.append(Lens(f=18, diameter=5.0, label='Tube Lens'))
+path.append(Space(d=18))
+path.figure.dpiFactor = 0  # try with 0, 0.5 and 1
+path.display()
