236f2e0dc06b5b82a558ed11f92a7b2091b50bc0
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-07-16 09:41:35 -0400

    Update Twophotondescanneddetector.py

--- examples/Twophotondescanneddetector.py
+++ examples/Twophotondescanneddetector.py
@@ -8,16 +8,27 @@ The size of the diffused light dictates which lenses and detector size to choose
 Therefore,  it is of great importance to find a well-sized detector that fits the optical system.  
 """
 
+
+
 nRays = 100000
 minHeight=-0.5
 maxHeight=0.5
 inputRays = RandomLambertianRays(yMax=maxHeight, yMin=minHeight, maxCount=nRays)
 
+# Defines the path. d1 and d2 are the diameter of the lenses, fl1 and fl2 are the focal lengths and d3 is the diameter of the aperture.
+def imagingPath(f1=10, d1=10, f2=10, d2=10, d3=10, title=""):
+    
+    path = ImagingPath()
+    path.label=title
+    path.append(System4f(f1=fl1, diameter1=d1, f2=fl2, diameter2=d2))
+    path.append(Aperture(diameter=d3, label='Camera'))
+    
+    return path
+
+# Three paths with different sets of lens and aperture.
 
 ###
-path1=ImagingPath()
-path1.append(System4f(f1=75,diameter1=50,f2=75,diameter2=75))
-path1.append(Aperture(diameter=0.5, label='Camera'))
+path1 = imagingPath(fl1=75, fl2=75, d1=50, d2=75, d3=0.5, title="")
 outputRays1 = path1.traceManyThrough(inputRays, progress=False)
 efficiency1 = 100*outputRays1.count/inputRays.count
 path1.display(limitObjectToFieldOfView=False, onlyPrincipalAndAxialRays=True)
@@ -25,10 +36,7 @@ outputRays1.display("Output profile {0:.0f}% efficiency".format(efficiency1), sh
 print(efficiency1)
 
 ###
-###
-path2=ImagingPath()
-path2.append(System4f(f1=50,diameter1=25,f2=50,diameter2=50))
-path2.append(Aperture(diameter=0.5, label='Camera'))
+path2 = imagingPath(fl1=50, fl2=50, d1=25, d2=50, d3=0.5, title="")
 outputRays2 = path2.traceManyThrough(inputRays, progress=False)
 efficiency2 = 100*outputRays2.count/inputRays.count
 path2.display(limitObjectToFieldOfView=False, onlyPrincipalAndAxialRays=True)
@@ -36,10 +44,7 @@ outputRays2.display("Output profile {0:.0f}% efficiency".format(efficiency2), sh
 print(efficiency2)
 
 ###
-###
-path3=ImagingPath()
-path3.append(System4f(f1=50,diameter1=25,f2=50,diameter2=50))
-path3.append(Aperture(diameter=1, label='Camera'))
+path3 = imagingPath(fl1=50, fl2=50, d1=25, d2=50, d3=1, title="")
 outputRays3 = path3.traceManyThrough(inputRays, progress=False)
 efficiency3 = 100*outputRays3.count/inputRays.count
 path3.display(limitObjectToFieldOfView=False, onlyPrincipalAndAxialRays=True)
