3aea0c7f96778ea7885706cdbfdb6bfada08187a
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-07-16 09:01:27 -0400

    Update twophotondescanneddetector.py

--- examples/Twophotondescanneddetector.py
+++ examples/Twophotondescanneddetector.py
@@ -13,15 +13,11 @@ minHeight=-0.5
 maxHeight=0.5
 inputRays = RandomLambertianRays(yMax=maxHeight, yMin=minHeight, maxCount=nRays)
 
-def imagingPath(f1=75,f2=75,d1=50, d2=50, title=""):
-    path = ImagingPath()
-    path.label=title
-    path.append(System4f(f1=f1, diameter1=d1, f2=f2, diameter2=d2))
-    path.append(Aperture(diameter=10, label='Camera'))
-    return path
 
 ###
-path1 = imagingPath(f1=75,d1=50,f2=75,d2=75)
+path1=ImagingPath()
+path1 = ImagingPath(f1=75,d1=50,f2=75,d2=75)
+path1.append(Aperture(diameter=0.5, label='Camera'))
 outputRays1 = path1.traceManyThrough(inputRays, progress=False)
 efficiency1 = 100*outputRays1.count/inputRays.count
 path1.display(limitObjectToFieldOfView=False, onlyPrincipalAndAxialRays=True)
@@ -29,7 +25,9 @@ outputRays1.display("Output profile {0:.0f}% efficiency".format(efficiency1), sh
 print(efficiency1)
 
 ###
-path2 = imagingPath(f1=50,d1=25,f2=50,d2=50)
+###
+path2=ImagingPath()
+path2 = ImagingPath(f1=50,d1=25,f2=50,d2=50)
 path2.append(Aperture(diameter=0.5, label='Camera'))
 outputRays2 = path2.traceManyThrough(inputRays, progress=False)
 efficiency2 = 100*outputRays2.count/inputRays.count
@@ -38,7 +36,10 @@ outputRays2.display("Output profile {0:.0f}% efficiency".format(efficiency2), sh
 print(efficiency2)
 
 ###
-path3 = imagingPath(f1=50,d1=25,f2=50,d2=50)
+###
+path3=ImagingPath()
+path3 = ImagingPath(f1=50,d1=25,f2=50,d2=50)
+path3.append(Aperture(diameter=1, label='Camera'))
 outputRays3 = path3.traceManyThrough(inputRays, progress=False)
 efficiency3 = 100*outputRays3.count/inputRays.count
 path3.display(limitObjectToFieldOfView=False, onlyPrincipalAndAxialRays=True)
