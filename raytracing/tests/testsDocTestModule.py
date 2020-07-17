a352cbded2cfc703eede86df8080953b786b10bb
ElaheParham <59700098+ElaheParham@users.noreply.github.com>
2020-07-17 11:57:02 -0400

    Updated documentation and doctest file

--- raytracing/tests/testsDocTestModule.py
+++ raytracing/tests/testsDocTestModule.py
@@ -4,21 +4,21 @@ import raytracing
 
 ## if a new python file is added to the module, please add it in a new line
 
-# doctest.testmod(m=raytracing.axicon,verbose=True)
-# doctest.testmod(m=raytracing.components,verbose=True)
-# doctest.testmod(m=raytracing.eo,verbose=True)
-# doctest.testmod(m=raytracing.figure,verbose=True)
-# doctest.testmod(m=raytracing.gaussianbeam,verbose=True)
-# doctest.testmod(m=raytracing.imagingpath,verbose=True)
-# doctest.testmod(m=raytracing.lasercavity,verbose=True)
-# doctest.testmod(m=raytracing.laserpath,verbose=True)
-# doctest.testmod(m=raytracing.materials,verbose=True)
-# doctest.testmod(m=raytracing.matrix,verbose=False)
-# doctest.testmod(m=raytracing.matrixgroup,verbose=False)
-# doctest.testmod(m=raytracing.ray,verbose=False)
-doctest.testmod(m=raytracing.rays,verbose=True)
-# doctest.testmod(m=raytracing.specialtylenses,verbose=True)
-# doctest.testmod(m=raytracing.utils,verbose=True)
+doctest.testmod(m=raytracing.axicon,verbose=False)
+doctest.testmod(m=raytracing.components,verbose=False)
+doctest.testmod(m=raytracing.eo,verbose=False)
+doctest.testmod(m=raytracing.figure,verbose=False)
+doctest.testmod(m=raytracing.gaussianbeam,verbose=False)
+doctest.testmod(m=raytracing.imagingpath,verbose=False)
+doctest.testmod(m=raytracing.lasercavity,verbose=False)
+doctest.testmod(m=raytracing.laserpath,verbose=False)
+doctest.testmod(m=raytracing.materials,verbose=False)
+doctest.testmod(m=raytracing.matrix,verbose=False)
+doctest.testmod(m=raytracing.matrixgroup,verbose=False)
+doctest.testmod(m=raytracing.ray,verbose=False)
+doctest.testmod(m=raytracing.rays,verbose=False)
+doctest.testmod(m=raytracing.specialtylenses,verbose=False)
+doctest.testmod(m=raytracing.utils,verbose=False)
 
 
 
