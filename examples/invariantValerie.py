f10141269d6c5f3e80d7264ad015664e4338580a
Valérie <valerie.pineau-noel.1@ulaval.ca>
2020-07-24 15:59:39 -0400

    Update invariantValerie.py

--- examples/invariantValerie.py
+++ examples/invariantValerie.py
@@ -2,11 +2,11 @@ import envexamples  # modifies path
 from raytracing import *
 
 path = ImagingPath()
-path.append(System4f(f1=10, diameter1=50, f2=20, diameter2=50))
+path.append(System4f(f1=10, diameter1=25.4, f2=20, diameter2=25.4))
 path.reportEfficiency()
 path.display()
 
 path2 = ImagingPath()
-path2.append(System4f(f1=10, diameter1=50, f2=20, diameter2=20))
+path2.append(System4f(f1=10, diameter1=12.7, f2=20, diameter2=25.4))
 path2.reportEfficiency()
 path2.display()
