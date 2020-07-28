37e871849c89385082e7b9e6354c875f0831e621
Valérie <valerie.pineau-noel.1@ulaval.ca>
2020-07-28 15:56:07 -0400

    invariantValerie.py added description to the code

--- examples/invariantValerie.py
+++ examples/invariantValerie.py
@@ -1,6 +1,14 @@
 import envexamples  # modifies path
 from raytracing import *
 
+"""
+The Lagrange invariant is a constant defining the collection efficiency of an optical system. The Lagrange 
+invariant is calculated using the principal and axial rays, whether the optical invariant is calculated with
+anyother combination of rays. This code uses the optical invariant to characterise the ray transmission in a
+4f system and shows that the optical invariant is greatly affected by the used optics. Indeed, changing the
+diameter of the first lens affects the number of detected rays at the imaged plane. 
+"""
+
 path = ImagingPath()
 path.append(System4f(f1=10, diameter1=25.4, f2=20, diameter2=25.4))
 path.reportEfficiency()
