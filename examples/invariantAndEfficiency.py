287cfa10f48717d8ad7cafb0d5541af1fd3a9914
Valérie <valerie.pineau-noel.1@ulaval.ca>
2020-07-29 09:50:02 -0400

    invariantAndEfficiency.py added interactive=False in display()

--- examples/invariantAndEfficiency.py
+++ examples/invariantAndEfficiency.py
@@ -12,9 +12,9 @@ diameter of the first lens affects the number of detected rays at the imaged pla
 path = ImagingPath()
 path.append(System4f(f1=10, diameter1=25.4, f2=20, diameter2=25.4))
 path.reportEfficiency()
-path.display()
+path.display(interactive=False)
 
 path2 = ImagingPath()
 path2.append(System4f(f1=10, diameter1=12.7, f2=20, diameter2=25.4))
 path2.reportEfficiency()
-path2.display()
+path2.display(interactive=False)
