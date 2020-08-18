18de5c1a0959d164340985b5377b90d0db272c11
Valérie <valerie.pineau-noel.1@ulaval.ca>
2020-08-18 12:01:58 -0400

    invariantAndEfficiency.py added path.design(pathScale=1.7)

--- examples/invariantAndEfficiency.py
+++ examples/invariantAndEfficiency.py
@@ -10,11 +10,13 @@ diameter of the first lens affects the number of detected rays at the imaged pla
 """
 
 path = ImagingPath()
+path.design(fontScale=1.7)
 path.append(System4f(f1=10, diameter1=25.4, f2=20, diameter2=25.4))
 path.reportEfficiency()
 path.display(interactive=False)
 
 path2 = ImagingPath()
+path2.design(fontScale=1.5)
 path2.append(System4f(f1=10, diameter1=12.7, f2=20, diameter2=25.4))
 path2.reportEfficiency()
 path2.display(interactive=False)
