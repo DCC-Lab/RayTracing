4d9b8f08c59ec06fb6c4a4e547aa14ad7578a125
Shadi Masoumi <59581437+shadimasoumi@users.noreply.github.com>
2020-08-19 10:40:39 -0400

    Update LSMConfocalpinhole.py

--- examples/LSMConfocalpinhole.py
+++ examples/LSMConfocalpinhole.py
@@ -11,6 +11,9 @@ import numpy as np
 
 
 """
+"""
+All the 1 indices relates to the Laser scanning calculations while 2 realtes to the Confocal pinhole side.
+"""
 # List of the scan angle of the ray making it throught the system. 
 thetas = []
 
@@ -41,7 +44,7 @@ scanRays = UniformRays(yMax=0, thetaMax=scanAngle, M=1, N=nRays)
 inputRays = RandomUniformRays(yMax=focalRadius, yMin=-focalRadius, maxCount=nRays)
 
 # Focal length of the objective
-objFocalLength = 5
+focalSpotPosition=objFocalLength = 5
 
 class UISUPLAPO60XW(Objective):
 
@@ -56,7 +59,7 @@ class UISUPLAPO60XW(Objective):
                                          label='UISUPLAPO60XW Objective')
 
 
-def illuminationPath():
+def path():
 
     illumination1 = ImagingPath()
     # The object in this situation is the laser beam at the scanning element. 
@@ -72,14 +75,96 @@ def illuminationPath():
 
     return illumination1
 
+def illuminationPath():
+    
+    path1 = illuminationPath()
+    outputRays1 = path1.traceManyThrough(scanRays)
+    for i in range(len(outputRays1)):
+        thetas.append(scanRays[i].theta*180/np.pi)
+        positions1.append(outputRays1[i].y*1000)
+        scanRays.displayProgress()
 
-path1 = illuminationPath()
-outputRays1 = path1.traceManyThrough(scanRays)
-for i in range(len(outputRays1)):
-    thetas.append(scanRays[i].theta*180/np.pi)
-    positions1.append(outputRays1[i].y*1000)
+def path(focalSpotPosition=objFocalLength): 
+    illumination2 = ImagingPath()
+    illumination2.append(Space(d=focalSpotPosition))
+    illumination2.append(Lens(f=objFocalLength))
+    illumination2.append(Space(d=105))
+    illumination2.append(Lens(f=100))
+    illumination2.append(Space(d=100))
+    illumination2.append(System4f(f1=100, f2=75))
+    illumination2.append(System4f(f1=40, f2=50))  # Path finishes at the pinhole position
 
-    scanRays.displayProgress()
+    return illumination2
+
+def optimalPinholeSize():
+    """
+    Finds the magnification of the optical path and use it to find the optimal pinhole size when the focal spot is at one
+    focal length distance of the objective. 
+
+    Return
+    -------
+        pinholeIdeal : Float
+            Returns the optimal pinhole size
+    """
+
+    # Dictionnary of the position and magnification of all conjugate planes of the focal spot. 
+    planes = path().intermediateConjugates()
+    # The last conjugate plane is the pinhole. The magnification of this position is saved in mag. 
+    mag = planes[-1][1]
+    # Calculates the pinhole size that fits perfectly the focal spot diameter.
+    pinholeIdeal = abs(mag * (focalRadius * 2))
+
+    return pinholeIdeal
+
+
+def illuminationPath(pinholeFactor=None, focalSpotPosition=None):
+    """
+    Determines the amount of rays emitted from the object that are detected at the pinhole plane. 
+
+    Parameter
+    ---------
+        pinholeFactor : Float
+            Factor changing the pinhole size according to the ideal pinhole size. 
+
+        focalSpotPosition : float
+            Position of the focal spot according to the objective (first lens)
+
+    Returns
+    -------
+        illumination : object of ImagingPath class.
+            Returns the illumination path
+    """
+
+    illumination2 = path(focalSpotPosition)
+
+    pinholeSize = optimalPinholeSize() * pinholeFactor
+    illumination2.append(Aperture(diameter=pinholeSize))
+
+    # Counts how many rays make it through the pinhole
+    outputRays2 = illumination2.traceManyThroughInParallel(inputRays, progress=False)
+
+    return outputRays2.count / inputRays.count
+
+
+for pinhole in pinholeModifier:
+    print("\nComputing transmission for pinhole size {0:0.1f}".format(pinhole))
+
+    efficiencyValues = []
+    for z in positions:
+        print(".",end='')
+        newPosition = 5 + (z * 0.000001)
+        efficiency = illuminationPath(pinholeFactor=pinhole, focalSpotPosition=newPosition)
+        efficiencyValues.append(efficiency)
+    pinholeModifier[pinhole] = efficiencyValues
+
+
+plt.plot(positions, pinholeModifier[1 / 3], 'k:', label='Small pinhole', linestyle='dashed')
+plt.plot(positions, pinholeModifier[1], 'k-', label='Ideal pinhole')
+plt.plot(positions, pinholeModifier[3], 'k--', label='Large pinhole', linestyle='dotted')
+plt.ylabel('Transmission efficiency')
+plt.xlabel('Position of the focal spot (nm)')
+plt.legend()
+plt.show()
 
 plt.plot(thetas,positions1)
 plt.xlabel('Scan angle (degrees)')
