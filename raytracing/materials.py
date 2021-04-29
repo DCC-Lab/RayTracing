045cfaa8be8ce0d82ff5f26a34d2d538dc4d1ff9
Daniel <dccote@cervo.ulaval.ca>
2021-04-29 18:51:02 -0400

    Abbe numbers added

--- raytracing/materials.py
+++ raytracing/materials.py
@@ -36,6 +36,15 @@ class Material:
             return matchObj.groups(1)[0]
         else:
             return "Unknown"
+    @classmethod
+    def findMaterial(cls, n, wavelength, tolerance=0.01):
+        match = []
+        for materialName in cls.all():
+            mat = globals()[materialName]()
+            nmat = mat.n(wavelength)
+            if abs(n-nmat) < tolerance:
+                match.append((materialName, nmat))
+        return match
 
 class Air(Material):
     @classmethod
@@ -52,6 +61,10 @@ class N_BK7(Material):
 
         n=(1+1.03961212/(1-0.00600069867/x**2)+0.231792344/(1-0.0200179144/x**2)+1.01046945/(1-103.560653/x**2))**.5
         return n
+    
+    @classmethod
+    def abbeNumber(cls):
+        return 64.17
 
 
 class N_SF2(Material):
@@ -64,6 +77,10 @@ class N_SF2(Material):
         n=(1+1.47343127/(1-0.0109019098/x**2)+0.163681849/(1-0.0585683687/x**2)+1.36920899/(1-127.404933/x**2))**.5
         return n
 
+    @classmethod
+    def abbeNumber(cls):
+        return 33.82
+
 class N_SF8(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/schott/N-SF8.html """
     @classmethod
@@ -73,6 +90,10 @@ class N_SF8(Material):
         x = wavelength
         n=(1+1.55075812/(1-0.0114338344/x**2)+0.209816918/(1-0.0582725652/x**2)+1.46205491/(1-133.24165/x**2))**.5
         return n
+
+    @classmethod
+    def abbeNumber(cls):
+        return 31.31
 
 
 class SF2(Material):
@@ -85,6 +106,10 @@ class SF2(Material):
         n=(1+1.40301821/(1-0.0105795466/x**2)+0.231767504/(1-0.0493226978/x**2)+0.939056586/(1-112.405955/x**2))**.5
         return n
 
+    @classmethod
+    def abbeNumber(cls):
+        return 33.85
+
 
 class SF5(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/schott/SF5.html """
@@ -95,6 +120,10 @@ class SF5(Material):
         x = wavelength
         n=(1+1.46141885/(1-0.0111826126/x**2)+0.247713019/(1-0.0508594669/x**2)+0.949995832/(1-112.041888/x**2))**.5
         return n
+
+    @classmethod
+    def abbeNumber(cls):
+        return 32.21
 
 
 class N_SF5(Material):
@@ -107,6 +136,9 @@ class N_SF5(Material):
         n=(1+1.52481889/(1-0.011254756/x**2)+0.187085527/(1-0.0588995392/x**2)+1.42729015/(1-129.141675/x**2))**.5
         return n
 
+    def abbeNumber(cls):
+        return 32.25
+
 
 class N_SF6HT(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/schott/N-SF6HT.html """
@@ -118,6 +150,8 @@ class N_SF6HT(Material):
         n=(1+1.77931763/(1-0.0133714182/x**2)+0.338149866/(1-0.0617533621/x**2)+2.08734474/(1-174.01759/x**2))**.5
         return n
 
+    def abbeNumber(cls):
+        return 25.36
 
 class N_SF10(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/schott/N-SF10.html """
@@ -129,6 +163,8 @@ class N_SF10(Material):
         n=(1+1.62153902/(1-0.0122241457/x**2)+0.256287842/(1-0.0595736775/x**2)+1.64447552/(1-147.468793/x**2))**.5
         return n
 
+    def abbeNumber(cls):
+        return 28.53
 
 class N_SF11(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/schott/N-SF11.html """
@@ -140,6 +176,9 @@ class N_SF11(Material):
         n=(1+1.73759695/(1-0.013188707/x**2)+0.313747346/(1-0.0623068142/x**2)+1.89878101/(1-155.23629/x**2))**.5
         return n
 
+    def abbeNumber(cls):
+        return 25.68
+
 class N_SF57(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/schott/N-SF57.html """
     @classmethod
@@ -149,6 +188,9 @@ class N_SF57(Material):
         x = wavelength
         n=(1+1.87543831/(1-0.0141749518/x**2)+0.37375749/(1-0.0640509927/x**2)+2.30001797/(1-177.389795/x**2))**.5
         return n
+
+    def abbeNumber(cls):
+        return 23.78
 
 class N_BAF10(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/schott/N-BAF10.html """
@@ -160,6 +202,8 @@ class N_BAF10(Material):
         n=(1+1.5851495/(1-0.00926681282/x**2)+0.143559385/(1-0.0424489805/x**2)+1.08521269/(1-105.613573/x**2))**.5
         return n
 
+    def abbeNumber(cls):
+        return 47.11
 
 class E_BAF11(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/hikari/E-BAF11.html """
@@ -171,6 +215,8 @@ class E_BAF11(Material):
         n=(2.71954649-0.0100472501*x**2+0.0200301385*x**-2+0.000465868302*x**-4-7.51633336e-06*x**-6+1.77544989e-06*x**-8)**.5
         return n
 
+    def abbeNumber(cls):
+        return 46.48
 
 class N_BAK1(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/schott/N-BAK1.html """
@@ -182,6 +228,8 @@ class N_BAK1(Material):
         n=(1+1.12365662/(1-0.00644742752/x**2)+0.309276848/(1-0.0222284402/x**2)+0.881511957/(1-107.297751/x**2))**.5
         return n
 
+    def abbeNumber(cls):
+        return 57.55
 
 class N_BAK4(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/schott/N-BAK4.html """
@@ -193,6 +241,8 @@ class N_BAK4(Material):
         n=(1+1.28834642/(1-0.00779980626/x**2)+0.132817724/(1-0.0315631177/x**2)+0.945395373/(1-105.965875/x**2))**.5
         return n
 
+    def abbeNumber(cls):
+        return 55.97
 
 class FK51A(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/schott/N-FK51A.html """
@@ -203,6 +253,9 @@ class FK51A(Material):
         x = wavelength
         n=(1+0.971247817/(1-0.00472301995/x**2)+0.216901417/(1-0.0153575612/x**2)+0.904651666/(1-168.68133/x**2))**.5
         return n
+
+    def abbeNumber(cls):
+        return 84.47
 
 
 class LAFN7(Material):
@@ -215,6 +268,8 @@ class LAFN7(Material):
         n=(1+1.66842615/(1-0.0103159999/x**2)+0.298512803/(1-0.0469216348/x**2)+1.0774376/(1-82.5078509/x**2))**.5
         return n
 
+    def abbeNumber(cls):
+        return 34.95
 
 class N_LASF9(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/schott/N-LASF9.html """
@@ -226,6 +281,8 @@ class N_LASF9(Material):
         n=(1+2.00029547/(1-0.0121426017/x**2)+0.298926886/(1-0.0538736236/x**2)+1.80691843/(1-156.530829/x**2))**.5
         return n
 
+    def abbeNumber(cls):
+        return 32.17
 
 class N_LAK22(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/schott/N-LAK22.html """
@@ -236,6 +293,9 @@ class N_LAK22(Material):
         x = wavelength
         n=(1+1.14229781/(1-0.00585778594/x**2)+0.535138441/(1-0.0198546147/x**2)+1.04088385/(1-100.834017/x**2))**.5
         return n
+
+    def abbeNumber(cls):
+        return 55.89
 
 
 class N_SSK5(Material):
@@ -248,6 +308,8 @@ class N_SSK5(Material):
         n=(1+1.59222659/(1-0.00920284626/x**2)+0.103520774/(1-0.0423530072/x**2)+1.05174016/(1-106.927374/x**2))**.5
         return n
 
+    def abbeNumber(cls):
+        return 50.88
 
 class E_FD10(Material):
     """ All data from https://refractiveindex.info/tmp/data/glass/hoya/E-FD10.html """
@@ -259,6 +321,8 @@ class E_FD10(Material):
         n=(2.881518-0.013228312*x**2+0.03145559*x**-2+0.0026851666*x**-4-0.00022577544*x**-6+2.4693268e-05*x**-8)**.5
         return n
 
+    def abbeNumber(cls):
+        return 28.32
 
 class FusedSilica(Material):
     """ All data from https://refractiveindex.info/tmp/data/main/SiO2/Malitson.html """
@@ -269,3 +333,6 @@ class FusedSilica(Material):
         x = wavelength
         n=(1+0.6961663/(1-(0.0684043/x)**2)+0.4079426/(1-(0.1162414/x)**2)+0.8974794/(1-(9.896161/x)**2))**.5
         return n
+
+    def abbeNumber(cls):
+        return 67.82
