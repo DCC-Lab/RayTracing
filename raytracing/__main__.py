26056888b6e3e960d71e16b640e92f02c24195bc
Daniel <dccote@cervo.ulaval.ca>
2021-04-30 22:55:51 -0400

    Ore improvment

--- raytracing/__main__.py
+++ raytracing/__main__.py
@@ -15,11 +15,7 @@ import argparse
 import re
 
 from . import examples # __all__ will gather all example files dynamically
-exampleScriptNames = examples.__all__
-print(exampleScriptNames)
-print(examples.ex01)
 
-exit(0)
 # We start by figuring out what the user really wants. If they don't know,
 # we offer some help
 ap = argparse.ArgumentParser(prog='python -m raytracing')
@@ -31,7 +27,7 @@ ap.add_argument("-l", "--list", required=False, action='store_const',
                 const=True, help="List all the accessible examples")
 
 args = vars(ap.parse_args())
-examples = args['examples']
+showExamples = args['examples']
 printClasses = args['classes']
 listExamples = args['list']
 
@@ -41,15 +37,13 @@ if printClasses:
     exit(0)
 elif listExamples:
     # List example code
-    for i, name in enumerate(exampleScriptNames):
-        print(name, globals())
-        mod = [name]
-        print("{0:2d}. {1} {2}".format(i+1, name, mod))
+    for i, entry in enumerate(examples.all):
+        print("{0:2d}. {1}.py {2}".format(i+1, entry["name"], entry["title"]))
     exit(0)
 else:
     # Run examples
-    for scriptName in exampleScripts:
-        scriptPath = "{0}/{1}".format(examplesDir, scriptName)
-        print(pythonExec)
-        os.system('{0} {1}'.format(pythonExec, scriptPath))
+    for i in showExamples:
+        print(i)
+        entry = examples.all[i]
+        entry["code"]()
 
--- raytracing/examples/__init__.py
+++ raytracing/examples/__init__.py
@@ -4,9 +4,15 @@ import importlib
 
 topDir      = os.path.dirname(os.path.realpath(__file__))
 allFiles    = os.listdir(topDir)
+allFiles.sort()
 
+all = []
 for file in allFiles:
     matchObj = re.match(r'^(ex\d+)\.py$', file)
     if matchObj:
-        importlib.import_module(".{0}".format(matchObj.group(1)),package="examples")
-
+        name = matchObj.group(1)
+        module = importlib.import_module(".{0}".format(name),package="raytracing.examples")
+        all.append({"name":name, 
+                     "title":module.TITLE,
+                     "description":module.DESCRIPTION,
+                     "code":module.exempleCode})
