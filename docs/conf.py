626bc810bf598419d2d9c576c4a65abd1cd7f3d9
JLBegin <ludovick.begin@gmail.com>
2021-05-05 16:46:02 -0400

    try to fix unexpected section title with numpydoc extension

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,8 @@ autosummary_generate = True
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.napoleon', 'recommonmark', 'sphinx_rtd_theme']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.napoleon', 'sphinx_rtd_theme',
+              'recommonmark', 'numpydoc']
 autodoc_default_flags = ['members', 'show-inheritance']
 autodoc_default_options = {
     "members": True,
