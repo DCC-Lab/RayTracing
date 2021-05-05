f9dc13600a63eea8393afe5c0c7443f67be04d2a
JLBegin <ludovick.begin@gmail.com>
2021-05-05 16:54:23 -0400

    Revert "try to fix unexpected section title with numpydoc extension"
    This reverts commit 626bc810bf598419d2d9c576c4a65abd1cd7f3d9.

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,7 @@ autosummary_generate = True
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.napoleon', 'sphinx_rtd_theme',
-              'recommonmark', 'numpydoc']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary', 'sphinx.ext.napoleon', 'recommonmark', 'sphinx_rtd_theme']
 autodoc_default_flags = ['members', 'show-inheritance']
 autodoc_default_options = {
     "members": True,
