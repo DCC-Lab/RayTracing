6075b19a67acd783999430a1a0daf7b5515d46f0
Daniel <dccote@cervo.ulaval.ca>
2021-05-06 22:59:20 -0400

    Returned testsDocModule to original script.

--- raytracing/tests/testsDocTestModule.py
+++ raytracing/tests/testsDocTestModule.py
@@ -5,55 +5,22 @@ from unittest.mock import Mock, patch
 
 ## if a new python file is added to the module, please add it in a new line
 
+with patch('matplotlib.pyplot.show', new=Mock()):
+    doctest.testmod(m=raytracing.axicon,verbose=False)
+    doctest.testmod(m=raytracing.components,verbose=False)
+    doctest.testmod(m=raytracing.eo,verbose=False)
+    doctest.testmod(m=raytracing.figure,verbose=False)
+    doctest.testmod(m=raytracing.gaussianbeam,verbose=False)
+    doctest.testmod(m=raytracing.imagingpath,verbose=False)
+    doctest.testmod(m=raytracing.lasercavity,verbose=False)
+    doctest.testmod(m=raytracing.laserpath,verbose=False)
+    doctest.testmod(m=raytracing.materials,verbose=False)
+    doctest.testmod(m=raytracing.matrix,verbose=False)
+    doctest.testmod(m=raytracing.matrixgroup,verbose=False)
+    doctest.testmod(m=raytracing.ray,verbose=False)
+    doctest.testmod(m=raytracing.rays,verbose=False)
+    doctest.testmod(m=raytracing.specialtylenses,verbose=False)
+    doctest.testmod(m=raytracing.utils,verbose=False)
 
 
-class TestMatrix(envtest.RaytracingTestCase):
-    def test1(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.axicon,verbose=False)
-    def test2(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.components,verbose=False)
-    def test3(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.eo,verbose=False)
-    def test4(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.figure,verbose=False)
-    def test5(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.gaussianbeam,verbose=False)
-    def test6(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.imagingpath,verbose=False)
-    def test7(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.lasercavity,verbose=False)
-    def test8(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.laserpath,verbose=False)
-    def test9(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.materials,verbose=False)
-    def test10(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.matrix,verbose=False)
-    def test11(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.matrixgroup,verbose=False)
-    def test12(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.ray,verbose=False)
-    def test13(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.rays,verbose=False)
-    def test14(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.specialtylenses,verbose=False)
-    def test15(self):
-        with patch('matplotlib.pyplot.show', new=Mock()):
-            doctest.testmod(m=raytracing.utils,verbose=False)
-
-if __name__ == '__main__':
-    envtest.main()
 
