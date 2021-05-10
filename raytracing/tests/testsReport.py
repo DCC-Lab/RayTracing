ae56de57ad29aa3512fa55d1dc42ab2de9756973
Daniel <dccote@cervo.ulaval.ca>
2021-05-09 22:46:16 -0400

    Test reports is better: we validate the length of the report
    ALso wrote functions for quickly patching matplotlib

--- raytracing/tests/testsReport.py
+++ raytracing/tests/testsReport.py
@@ -3,17 +3,61 @@ from raytracing import *
 from raytracing.zemax import ZMXReader
 from numpy import linspace, pi
 from raytracing.materials import *
+import io
+from contextlib import redirect_stdout
 
 inf = float("+inf")
 
 class TestReport(envtest.RaytracingTestCase):
-    def testCreation(self):
+    stdout = io.StringIO()
+    
+    def setUp(self):
+        self.stdout = io.StringIO()
+
+    def testCreationNoASNoFS(self):
         f = 50
         path = ImagingPath()
         path.append(Space(d=f))
         path.append(Lens(f=f))
         path.append(Space(d=f))
-        self.assertDoesNotRaise(path.reportEfficiency)
+        with redirect_stdout(self.stdout):
+            path.reportEfficiency()
+
+        self.assertTrue(len(self.stdout.getvalue()) < 100)
+        position, diameter = path.fieldStop()
+        self.assertIsNone(position)
+        self.assertTrue(diameter == float("+inf"))
+
+    @envtest.patchMatplotLib()
+    def testCreationASNoFS(self):
+        f = 50
+        path = ImagingPath()
+        path.append(Space(d=f))
+        path.append(Lens(f=f,diameter=10))
+        path.append(Space(d=f))
+        with redirect_stdout(self.stdout):
+            path.reportEfficiency()
+
+        self.assertTrue(len(self.stdout.getvalue()) < 100)
+        position, diameter = path.fieldStop()
+        self.assertIsNone(position)
+        self.assertTrue(diameter == float("+inf"))
+
+    @envtest.patchMatplotLib()
+    def testCreationASFS(self):
+        f = 50
+        path = ImagingPath()
+        path.append(Space(d=f))
+        path.append(Lens(f=f, diameter=6))
+        path.append(Space(d=f))
+        path.append(Lens(f=f, diameter=6))
+        with redirect_stdout(self.stdout):
+            path.reportEfficiency()
+
+        self.assertTrue(len(self.stdout.getvalue()) > 100)
+        position, diameter = path.fieldStop()
+        self.assertIsNotNone(position)
+        self.assertTrue(diameter != float("+inf"))
 
 
 if __name__ == '__main__':
