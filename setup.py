e300834573cca498b624a33d3645f4d6793aebfe
Daniel <dccote@cervo.ulaval.ca>
2021-06-30 23:29:19 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
 
         'Operating System :: OS Independent'
     ],
